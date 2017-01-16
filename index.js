6fd6d0186a4e35289ef7d39860eff66a6d6d5c7d
Jiang Sheng <gisonrg@gmail.com>
2017-01-17 03:05:31 +0800

    Adding “build” and “serve” command. Rewrite using ES6 Promise.

--- index.js
+++ index.js
@@ -5,14 +5,20 @@
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
-const fs = require('fs');
+const fs = require('fs-extra-promise');
 const path = require('path');
 const program = require('commander');
 const html = require('html');
+const liveServer = require('live-server');
+const chokidar = require('chokidar');
 
+const Site = require('./lib/Site');
 const MarkBind = require('markbind');
 
-let markbindParser = new MarkBind();
+const CLI_VERSION = require('./package.json').version;
+const ACCEPTED_COMMANDS = ['version', 'include', 'render', 'build', 'serve'];
+
+let markbinder = new MarkBind();
 
 clear();
 
@@ -24,72 +30,145 @@ function printLogo() {
   );
 }
 
+function printInfo(text) {
+  console.log(chalk.cyan(text));
+}
+
+function printError(text) {
+  console.log(chalk.red(text));
+}
+
+process.title = 'MarkBind';
+process.stdout.write(
+  String.fromCharCode(27) + ']0;' + 'MarkBind' + String.fromCharCode(7)
+);
+
 program
-  .version('0.1.0')
+  .allowUnknownOption()
+  .usage(' <command>');
+
+program
+  .version(CLI_VERSION)
   .option('-o, --output <path>', 'output file path')
-  .arguments('<file>')
   .action(function (file) {
-    console.log('Please first run include command, then run render command for the include result');
+    printInfo('Please first run include command, then run render command on the include result');
   });
 
 program
   .command('include <file>')
   .description('process all the include in the given file')
   .action(function (file) {
-    markbindParser.includeFile(path.resolve(process.cwd(), file), (error, result) => {
-      if (error) {
-        console.log(chalk.red('Error processing file including:'));
-        console.log(chalk.red(error.message));
-        return;
-      }
-      if (program.output) {
-        printLogo();
-        let outputPath = path.resolve(process.cwd(), program.output);
-        try {
-          fs.statSync(path.dirname(outputPath));
-        } catch (error) {
-          fs.mkdirSync(path.dirname(outputPath));
+    markbinder.includeFile(path.resolve(process.cwd(), file))
+      .then((result) => {
+        if (program.output) {
+          let outputPath = path.resolve(process.cwd(), program.output);
+          fs.outputFileSync(outputPath, result);
+          printLogo();
+          printInfo(`Result was written to ${outputPath}`);
+        } else {
+          console.log(result);
         }
-        console.log(chalk.cyan('Result was written to ' + outputPath));
-        fs.writeFileSync(outputPath, result);
-      } else {
-        console.log(result);
-      }
-    });
+      })
+      .catch((error) => {
+        printError('Error processing file including:');
+        printError(error.message);
+        return;
+      });
   });
 
 program
   .command('render <file>')
   .description('render the given file')
   .action(function (file) {
-    // TODO: need to check file existence
-    let output = program.output || 'output';
-    markbindParser.renderFile(path.resolve(process.cwd(), file), (error, result) => {
-      if (error) {
-        console.log(chalk.red('Error processing file rendering:'));
-        console.log(chalk.red(error.message));
-        return;
-      }
-      result = html.prettyPrint(result, {indent_size: 2});
-      if (program.output) {
-        printLogo();
-        let outputPath = path.resolve(process.cwd(), program.output);
-        try {
-          fs.statSync(path.dirname(outputPath));
-        } catch (error) {
-          fs.mkdirSync(path.dirname(outputPath));
+    markbinder.renderFile(path.resolve(process.cwd(), file))
+      .then((result) => {
+        result = html.prettyPrint(result, {indent_size: 2});
+        if (program.output) {
+          let outputPath = path.resolve(process.cwd(), program.output);
+          fs.outputFileSync(outputPath, result);
+          printLogo();
+          printInfo(`Result was written to ${outputPath}`);
+        } else {
+          console.log(result);
         }
-        console.log(chalk.cyan('Result was written to ' + outputPath));
-        fs.writeFileSync(outputPath, result);
-      } else {
-        console.log(result);
-      }
+      })
+      .catch((error) => {
+        printError('Error processing file rendering:');
+        printError(error.message);
+        return;
+      });
+  });
+
+program
+  .command('serve [root]')
+  .description('build then serve a website from a directory')
+  .action((root) => {
+    const rootFolder = path.resolve(root || process.cwd());
+    const outputFolder = path.join(rootFolder, '_site');
+
+    let site = new Site(rootFolder, outputFolder);
+
+    let changeHandler = (path) => {
+      printInfo(`Reload for file change: ${path}`);
+      return site.generate();
+    };
+
+    // TODO: optimize the regenerate logic (exclude the markbind asset)
+    site
+      .generate()
+      .then(() => {
+        var watcher = chokidar.watch(rootFolder, {
+          ignored: [outputFolder, /(^|[\/\\])\../],
+          ignoreInitial: true
+        });
+        watcher
+          .on('add', changeHandler)
+          .on('change', changeHandler)
+          .on('unlink', changeHandler);
+      })
+      .then(() => {
+        let server = liveServer.start({
+          open: false,
+          logLevel: 0,
+          root: outputFolder,
+        });
+        server.addListener('listening', function() {
+          var address = server.address();
+          var serveHost = address.address === '0.0.0.0' ? '127.0.0.1' : address.address;
+          var serveURL = 'http://' + serveHost + ':' + address.port;
+          printInfo(`Serving \"${outputFolder}\" at ${serveURL}`)
+          printInfo('Press CTRL+C to stop ...');
+        });
+        printLogo();
+      })
+      .catch((error) => {
+      printLogo();
+      printError(error.message);
     });
   });
 
-if (typeof cmdValue === 'undefined') {
-  // TODO: Handle illegal command
-  // process.exit(1);
-}
+program
+  .command('build [root] [output]')
+  .description('build a website')
+  .action((root, output) => {
+    const rootFolder = path.resolve(root || process.cwd());
+    const defaultOutputRoot = path.join(rootFolder, '_site');
+    const outputFolder = output ? path.resolve(process.cwd(), output) : defaultOutputRoot;
+
+    new Site(rootFolder, outputFolder)
+      .generate()
+      .then(() => {
+        printLogo();
+        printInfo('Build success!');
+      }).catch((error) => {
+        printLogo();
+        printError(error.message);
+      });
+  });
 
 program.parse(process.argv);
+
+if (!program.args.length || !ACCEPTED_COMMANDS.includes(process.argv[2])) {
+  printLogo();
+  program.help();
+}
