f8bd9b9bd5a2300b1c74fa97a22b95bc0340b06a
Damith C. Rajapakse <damith@gmail.com>
2018-03-08 17:53:52 +0800

    Site Update.

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+set -x
+rm -rf /c/Users/dcsdcr/AppData/Roaming/npm/node_modules/markbind-cli/node_modules/gh-pages/.cache
+markbind build
+markbind deploy
---
+++ markbind/js/setup.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+
+Vue.use(VueStrap);
+const vm = new Vue({
+  el: '#app',
+});
+VueStrap.installEvents(vm);
