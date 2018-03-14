84c3238e62f5a71f1c13e71b2ef334f1e7179636
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2018-03-14 11:41:31 +0800

    add comment to test.sh

--- test/test_site/test.sh
+++ test/test_site/test.sh
@@ -1,11 +1,14 @@
-#! /bin/bash
+#!/bin/bash
 
+# build site in test/test_site/_site
 node ../../index.js build
 
+# run our test script to compare html files
 node ./testUtil/test.js
 
 if [ $? -eq 0 ]
 then
+# if there were no diffs
     echo "Test result: PASSED"
     exit 0
 else
