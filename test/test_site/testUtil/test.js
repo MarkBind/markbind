03bff095ebb0dfffc9096b56b8895e3ef5aa9124
Aaron Chong <aaronjhchong@hotmail.com>
2018-04-18 00:39:07 +0800

    Refactor test for equality of siteData

--- test/test_site/testUtil/test.js
+++ test/test_site/testUtil/test.js
@@ -6,6 +6,10 @@ const walkSync = require('walk-sync');
 const _ = {};
 _.isEqual = require('lodash/isEqual');
 
+function readFileSync(...paths) {
+  return fs.readFileSync(path.resolve(...paths), 'utf8');
+}
+
 const expectedPaths = walkSync('./expected', { directories: false });
 const actualPaths = walkSync('./_site', { directories: false });
 
@@ -21,22 +25,22 @@ for (let i = 0; i < expectedPaths.length; i += 1) {
     throw new Error('Different files built');
   }
 
-  // compare html files and site data
-  if (path.parse(actualFilePath).ext === '.html' || actualFilePath.includes('siteData.json')) {
-    const resolvedExpectedFilePath = path.resolve('./expected', expectedFilePath);
-    const resolvedActualFilePath = path.resolve('./_site', actualFilePath);
-    const expected = fs.readFileSync(resolvedExpectedFilePath, 'utf8');
-    const actual = fs.readFileSync(resolvedActualFilePath, 'utf8');
-    if (actualFilePath.includes('siteData.json')) {
-      if (!_.isEqual(JSON.parse(expected), JSON.parse(actual))) {
-        throw new Error('Site data does not match with the expected file.');
-      }
-    } else {
-      try {
-        diffHtml(expected, actual);
-      } catch (err) {
-        throw new Error(`${err.message} in ${expectedFilePath}`);
-      }
+  const parsed = path.parse(actualFilePath);
+  if (parsed.ext === '.html') {
+    // compare html files
+    const expected = readFileSync('./expected', expectedFilePath);
+    const actual = readFileSync('./_site', actualFilePath);
+    try {
+      diffHtml(expected, actual);
+    } catch (err) {
+      throw new Error(`${err.message} in ${expectedFilePath}`);
+    }
+  } else if (parsed.base === 'siteData.json') {
+    // compare site data
+    const expected = readFileSync('./expected', expectedFilePath);
+    const actual = readFileSync('./_site', actualFilePath);
+    if (!_.isEqual(JSON.parse(expected), JSON.parse(actual))) {
+      throw new Error('Site data does not match with the expected file.');
     }
   }
 }
