99eb322285d057e03e65f6d45270ad2f03f4f596
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2018-04-20 00:33:22 +0800

    print the error

--- test/test_site/testUtil/diffHtml.js
+++ test/test_site/testUtil/diffHtml.js
@@ -90,6 +90,8 @@ const diffHtml = (expected, actual) => {
     }
 
     if (isDiff(part) && !insidePath) {
+      console.log(expected);
+      console.log(actual);
       throw new Error(`Diff outside path!: '${part.value}'`);
     } else if (isDiff(part) && !isPathSeparatorDiff(part.value)) {
       throw new Error(`Diff in path!: '${part.value}'`);
