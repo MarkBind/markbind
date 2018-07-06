c5bf1d86a8f8782e7f6202ab1eedc6482d14a13c
Tan Wang Leng <tanwangleng@outlook.com>
2018-07-06 14:36:29 +0800

    Site Update.

--- markbind/js/setup.js
+++ markbind/js/setup.js
@@ -2,15 +2,40 @@
 
 Vue.use(VueStrap);
 
+function setupSiteNav() {
+  // Add event listener for site-nav-btn to toggle itself and site navigation elements.
+  const siteNavBtn = document.getElementById('site-nav-btn');
+  if (siteNavBtn) {
+    siteNavBtn.addEventListener('click', function () {
+      this.classList.toggle('shift');
+      document.getElementById('site-nav').classList.toggle('open');
+      document.getElementById('site-nav-btn-wrap').classList.toggle('open');
+    });
+  }
+  // Creates event listener for all dropdown-btns in page.
+  Array.prototype.forEach.call(
+    document.getElementsByClassName('dropdown-btn'),
+    dropdownBtn => dropdownBtn.addEventListener('click', function () {
+      this.classList.toggle('dropdown-btn-open');
+      const dropdownContent = this.nextElementSibling;
+      const dropdownIcon = this.lastElementChild;
+      dropdownContent.classList.toggle('dropdown-container-open');
+      dropdownIcon.classList.toggle('rotate-icon');
+    }),
+  );
+}
+
 function setup() {
+  // eslint-disable-next-line no-unused-vars
   const vm = new Vue({
     el: '#app',
   });
-  VueStrap.installEvents(vm);
+  setupSiteNav();
 }
 
 function setupWithSearch(siteData) {
   const { searchbar } = VueStrap.components;
+  // eslint-disable-next-line no-unused-vars
   const vm = new Vue({
     el: '#app',
     components: {
@@ -29,7 +54,7 @@ function setupWithSearch(siteData) {
       },
     },
   });
-  VueStrap.installEvents(vm);
+  setupSiteNav();
 }
 
 jQuery.getJSON(`${baseUrl}/siteData.json`)
