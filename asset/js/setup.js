fa3be99ffdbd45f9863e92290151c11ab1a3ecd5
Jamos Tay <jamostay94@hotmail.com>
2018-10-31 01:00:44 +0800

    Make hover apply to wrapper

--- asset/js/setup.js
+++ asset/js/setup.js
@@ -16,13 +16,9 @@ function flattenModals() {
 }
 
 function setupAnchors() {
-  jQuery('h1, h2, h3, h4, h5, h6').each((index, heading) => {
-    jQuery(heading).on('mouseenter', function () {
-      jQuery(this).children('.fa.fa-anchor').show();
-    });
-    jQuery(heading).on('mouseleave', function () {
-      jQuery(this).children('.fa.fa-anchor').hide();
-    });
+  jQuery('h1, h2, h3, h4, h5, h6, .header-wrapper').each((index, heading) => {
+    jQuery(heading).on('mouseenter', () => jQuery(heading).find('.fa.fa-anchor').show());
+    jQuery(heading).on('mouseleave', () => jQuery(heading).find('.fa.fa-anchor').hide());
   });
   jQuery('.fa-anchor').each((index, anchor) => {
     jQuery(anchor).on('click', function () {
