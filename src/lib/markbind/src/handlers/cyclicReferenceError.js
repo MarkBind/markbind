fc8d567b5ef44a22a5027f4b6ad2ee8e76c6da75
Si Jie <sijie123@gmail.com>
2019-02-01 11:56:46 +0800

    CyclicReferenceError.js: Remove ERROR_MESSAGE
    ERROR_MESSAGE is redundant as it is only used in 1 place within the class.

--- src/lib/markbind/src/handlers/cyclicReferenceError.js
+++ src/lib/markbind/src/handlers/cyclicReferenceError.js
@@ -1,20 +1,10 @@
-const MAX_RECURSIVE_DEPTH = 200;
-const ERROR_MESSAGE = 'Cyclic reference detected.';
-
 class CyclicReferenceError extends Error {
-  static get MAX_RECURSIVE_DEPTH() {
-    return MAX_RECURSIVE_DEPTH;
-  }
-
-  static get ERROR_MESSAGE() {
-    return ERROR_MESSAGE;
-  }
-
   constructor(callStack) {
     super();
     const fileStack = callStack.slice(Math.max(callStack.length - 5, 0));
-    this.message = `${ERROR_MESSAGE}\n`
+    this.message = 'Cyclic reference detected.\n'
         + `Last 5 files processed:${'\n\t'}${fileStack.join('\n\t')}`;
   }
 }
+CyclicReferenceError.MAX_RECURSIVE_DEPTH = 200;
 module.exports = CyclicReferenceError;
