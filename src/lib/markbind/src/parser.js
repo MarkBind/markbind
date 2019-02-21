154e07c1e3c4a06145b3012507c16bbc75b83cc1
amad-person <e0072565@u.nus.edu>
2019-02-21 13:14:28 +0800

    Move second arg to new line in fileContent renderString

--- src/lib/markbind/src/parser.js
+++ src/lib/markbind/src/parser.js
@@ -505,7 +505,8 @@ Parser.prototype.includeFile = function (file, config) {
       const { parent, relative } = calculateNewBaseUrls(file, config.rootPath, config.baseUrlMap);
       const userDefinedVariables = config.userDefinedVariablesMap[path.resolve(parent, relative)];
       const pageVariables = extractPageVariables(path.basename(file), data, userDefinedVariables, {});
-      const fileContent = nunjucks.renderString(data, { ...pageVariables, ...userDefinedVariables },
+      const fileContent = nunjucks.renderString(data,
+                                                { ...pageVariables, ...userDefinedVariables },
                                                 { path: actualFilePath });
       const fileExt = utils.getExt(file);
       if (utils.isMarkdownFileExt(fileExt)) {
