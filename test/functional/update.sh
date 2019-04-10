040abb1d50ea6fd46f08d40797dcc96f4bd181be
Marvin Chin <marvinchin@u.nus.edu>
2019-04-10 18:07:52 +0800

    Add script to update expected files for test sites
    Developers have to update the expected folder for test sites by hand.
    Updating the expected folders for each site manually becomes especially
    unwieldy as the number of test sites increase.
    
    Let's have a script which automates the updating of expected folders in
    all of the test sites.

---
+++ test/functional/update.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+for site in $(cat test_sites);
+do
+    # print site name
+    echo "Updating $site"
+
+    # build site
+    node ../../index.js build "$site"
+
+    # replace the expected folder with the newly generated files
+    rm -rf $site/expected
+    cp -r $site/_site $site/expected
+done
+
+function cleanup_convert {
+    # delete generated files
+    rm -rf $site_convert/_site
+    rm -rf $site_convert/non_markbind_site/_markbind $site_convert/non_markbind_site/_site
+    rm -f $site_convert/non_markbind_site/about.md $site_convert/non_markbind_site/index.md "$site_convert"/non_markbind_site/site.json
+}
+
+for site_convert in $(cat test_convert_sites);
+do
+    # print site name
+    echo "Updating $site_convert test"
+
+    # set cleanup trap
+    trap cleanup_convert EXIT
+
+    # convert site
+    node ../../index.js init $site_convert/non_markbind_site -c
+
+    # build site
+    node ../../index.js build $site_convert/non_markbind_site
+
+    # replace the expected folder with the newly generated files
+    rm -rf $site_convert/expected
+    cp -r $site_convert/non_markbind_site/_site $site_convert/expected
+
+    # cleanup generated files
+    cleanup_convert
+
+    # remove trap
+    trap - EXIT
+done
+
+echo "Updated all test sites"
+exit 0
