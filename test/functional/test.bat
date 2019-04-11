b0ee8115af300dd384fd617302c26eabeb05fc81
amad-person <e0072565@u.nus.edu>
2019-04-11 15:25:08 +0800

    Bug fix: test.bat cleans up conversion files generated in FT even on failure

--- test/functional/test.bat
+++ test/functional/test.bat
@@ -32,15 +32,17 @@ for %%a in (%sites_convert%) do (
 
     node testUtil/test.js %%a
 
-    if errorlevel 1 (
-        echo Test %%a Failed
-        exit /b %errorlevel%
-    )
+    call set level=%%errorlevel%%
 
     rmdir /s /q %%a\_site
     rmdir /s /q %%a\non_markbind_site\_markbind
     rmdir /s /q %%a\non_markbind_site\_site
     del %%a\non_markbind_site\about.md %%a\non_markbind_site\index.md %%a\non_markbind_site\site.json
+
+    if %level%==1 (
+        echo Test %%a Failed
+        exit /b %level%
+    )
 )
 
 echo Test passed
