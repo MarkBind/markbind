151ac6a13d701ee92225a69240ac2f83c6342e3f
Marvin Chin <marvinchin@u.nus.edu>
2019-04-12 15:26:45 +0800

    Standardize test script implementation
    The implementation of the bash and batch test script differs slightly.
    The batch script implementation allows multiple test convert sites, but
    the bash script implementation only allows a single test convert site.
    
    Let's update the bash script to allow it to be extensible to multiple
    test convert sites.

--- test/functional/test.sh
+++ test/functional/test.sh
@@ -21,35 +21,43 @@ done
 
 function cleanup_convert {
     # delete generated files
-    rm -rf "$site_convert"/_site
-    rm -rf "$site_convert"/non_markbind_site/_markbind "$site_convert"/non_markbind_site/_site
-    rm "$site_convert"/non_markbind_site/about.md "$site_convert"/non_markbind_site/index.md "$site_convert"/non_markbind_site/site.json
+    rm -rf $site_convert/_site
+    rm -rf $site_convert/non_markbind_site/_markbind $site_convert/non_markbind_site/_site
+    rm -f $site_convert/non_markbind_site/about.md $site_convert/non_markbind_site/index.md $site_convert/non_markbind_site/site.json
 }
-trap cleanup_convert EXIT
 
-declare -r site_convert="test_site_convert"
+for site_convert in $(cat test_convert_sites);
+do
+    # print site name
+    echo "Running $site_convert test"
 
-# print site name
-echo
-echo "Running $site_convert test"
+    # set cleanup trap
+    trap cleanup_convert EXIT
 
-# convert site
-node ../../index.js init "$site_convert"/non_markbind_site -c
+    # convert site
+    node ../../index.js init $site_convert/non_markbind_site -c
 
-# build site
-node ../../index.js build "$site_convert"/non_markbind_site
+    # build site
+    node ../../index.js build $site_convert/non_markbind_site
 
-# copy generated site
-cp -r "$site_convert"/non_markbind_site/_site "$site_convert"
+    # copy generated site
+    cp -r $site_convert/non_markbind_site/_site $site_convert
 
-# run our test script to compare html files
-node testUtil/test.js "$site_convert"
+    # run our test script to compare html files
+    node testUtil/test.js $site_convert
 
-if [ $? -ne 0 ]
-then
-    echo "Test result: $site FAILED"
-    exit 1
-fi
+    if [ $? -ne 0 ]
+    then
+        echo "Test result: $site_convert FAILED"
+        exit 1
+    fi
+
+    # cleanup generated files
+    cleanup_convert
+
+    # remove trap
+    trap - EXIT
+done
 
 # if there were no diffs
 echo "Test result: PASSED"
