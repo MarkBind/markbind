92396862e208867b2e7f84227d927df568f5ef6d
Phang Chun Rong <crphang@gmail.com>
2019-08-06 20:50:28 +0800

    Add google analytics support (#929)

---
+++ src/plugins/googleAnalytics.js
@@ -0,0 +1,17 @@
+function getGoogleAnalyticsTrackingCode(pluginContext) {
+  return `
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=${pluginContext.trackingID}"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', '${pluginContext.trackingID}');
+    </script>`;
+}
+
+module.exports = {
+  // eslint-disable-next-line no-unused-vars
+  getScripts: (content, pluginContext, frontMatter, utils) => getGoogleAnalyticsTrackingCode(pluginContext),
+};
