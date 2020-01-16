0114e66f17aaba6fa1f242130f424120441756dd
ang-zeyu <angzeyu@gmail.com>
2020-01-16 20:28:21 +0800

    Implement per component markdown attribute parsing
    Markdown passed in attributes are being rendered in the client.
    
    This results in a dependency on the markdown parsing library
    (markdown-it) on the client, bloating the resulting client bundle size.
    It also slows webpage loading, especially on older devices.
    Moreover, /markbind already parses some amount of markdown in attributes
    for other purposes when building the site. This results in a less
    cohesive codebase.
    
    Let's move all markdown-it parsing from /vue-strap to /markbind,
    passing the rendered html into vue slots.
    Additionally, let's conveniently standardise the attribute naming for
    these components at the same time.

--- src/plugins/default/markbind-plugin-anchors.js
+++ src/plugins/default/markbind-plugin-anchors.js
@@ -1,5 +1,4 @@
 const cheerio = module.parent.require('cheerio');
-const md = require('./../../lib/markbind/src/lib/markdown-it');
 
 const {
   ANCHOR_HTML,
@@ -15,14 +14,6 @@ module.exports = {
     $(HEADER_TAGS).each((i, heading) => {
       if ($(heading).attr('id')) {
         $(heading).append(ANCHOR_HTML.replace('#', `#${$(heading).attr('id')}`));
-      }
-    });
-    $('panel[header]').each((i, panel) => {
-      const panelHeading = cheerio.load(md.render(panel.attribs.header), { xmlMode: false });
-      if (panelHeading(HEADER_TAGS).length >= 1) {
-        const headingId = $(panelHeading(HEADER_TAGS)[0]).attr('id');
-        const anchorIcon = ANCHOR_HTML.replace(/"/g, "'").replace('#', `#${headingId}`);
-        $(panel).attr('header', `${$(panel).attr('header')}${anchorIcon}`);
       }
     });
     return $.html();
