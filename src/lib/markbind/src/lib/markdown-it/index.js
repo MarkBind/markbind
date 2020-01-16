76abf9360ed84c1aaa910be3a16d6db9285f962f
ang-zeyu <angzeyu@gmail.com>
2020-01-16 19:43:42 +0800

    Import markdown-it parser variant from /vue-strap
    Markdown-it in /vue-strap uses different plugins for parsing
    its markdown content inside component attributes.
    
    In preparation for moving markdown-it parsing to /markbind, let's move
    the markdown-it variant from /vue-strap over to maintain parity with the
    past markdown rendering behaviour for attributes in vue components.
    Let's also restructure the directory hierarchy for markdown-it in here,
    to reduce code duplication between the two variants.

--- src/lib/markbind/src/lib/markdown-it/index.js
+++ src/lib/markbind/src/lib/markdown-it/index.js
@@ -8,16 +8,15 @@ const slugify = require('@sindresorhus/slugify');
 // markdown-it plugins
 markdownIt.use(require('markdown-it-mark'))
   .use(require('markdown-it-ins'))
-  .use(require('markdown-it-anchor'), {slugify: (str) => slugify(str, { decamelize: false })})
   .use(require('markdown-it-imsize'), {autofill: false})
   .use(require('markdown-it-table-of-contents'))
   .use(require('markdown-it-task-lists'), {enabled: true})
   .use(require('markdown-it-linkify-images'), {imgClass: 'img-fluid'})
   .use(require('markdown-it-attrs'))
-  .use(require('./markdown-it-dimmed'))
+  .use(require('../markdown-it-shared/markdown-it-dimmed'))
   .use(require('./markdown-it-radio-button'))
   .use(require('./markdown-it-block-embed'))
-  .use(require('./markdown-it-icons'))
+  .use(require('../markdown-it-shared/markdown-it-icons'))
   .use(require('./markdown-it-footnotes'));
 
 // fix link
@@ -85,21 +84,9 @@ markdownIt.renderer.rules.code_inline = (tokens, idx, options, env, slf) => {
   }
 };
 
-// fix emoji numbers
-const emojiData = require('markdown-it-emoji/lib/data/full.json');
-// Extend emoji here
-emojiData['zero'] = emojiData['0'] = '<img style="height: 1em;width: 1em;margin: 0 .05em 0 .1em;vertical-align: -0.1em;" src="https://assets-cdn.github.com/images/icons/emoji/unicode/0030-20e3.png">';
-emojiData['one'] = emojiData['1'] = '<img style="height: 1em;width: 1em;margin: 0 .05em 0 .1em;vertical-align: -0.1em;" src="https://assets-cdn.github.com/images/icons/emoji/unicode/0031-20e3.png">';
-emojiData['two'] = emojiData['2'] = '<img style="height: 1em;width: 1em;margin: 0 .05em 0 .1em;vertical-align: -0.1em;" src="https://assets-cdn.github.com/images/icons/emoji/unicode/0032-20e3.png">';
-emojiData['three'] = emojiData['3'] = '<img style="height: 1em;width: 1em;margin: 0 .05em 0 .1em;vertical-align: -0.1em;" src="https://assets-cdn.github.com/images/icons/emoji/unicode/0033-20e3.png">';
-emojiData['four'] = emojiData['4'] = '<img style="height: 1em;width: 1em;margin: 0 .05em 0 .1em;vertical-align: -0.1em;" src="https://assets-cdn.github.com/images/icons/emoji/unicode/0034-20e3.png">';
-emojiData['five'] = emojiData['5'] = '<img style="height: 1em;width: 1em;margin: 0 .05em 0 .1em;vertical-align: -0.1em;" src="https://assets-cdn.github.com/images/icons/emoji/unicode/0035-20e3.png">';
-emojiData['six'] = emojiData['6'] = '<img style="height: 1em;width: 1em;margin: 0 .05em 0 .1em;vertical-align: -0.1em;" src="https://assets-cdn.github.com/images/icons/emoji/unicode/0036-20e3.png">';
-emojiData['seven'] = emojiData['7'] = '<img style="height: 1em;width: 1em;margin: 0 .05em 0 .1em;vertical-align: -0.1em;" src="https://assets-cdn.github.com/images/icons/emoji/unicode/0037-20e3.png">';
-emojiData['eight'] = emojiData['8'] = '<img style="height: 1em;width: 1em;margin: 0 .05em 0 .1em;vertical-align: -0.1em;" src="https://assets-cdn.github.com/images/icons/emoji/unicode/0038-20e3.png">';
-emojiData['nine'] = emojiData['9'] = '<img style="height: 1em;width: 1em;margin: 0 .05em 0 .1em;vertical-align: -0.1em;" src="https://assets-cdn.github.com/images/icons/emoji/unicode/0039-20e3.png">';
+const fixedNumberEmojiDefs = require('../markdown-it-shared/markdown-it-emoji-fixed');
 markdownIt.use(require('markdown-it-emoji'), {
-  defs: emojiData
+  defs: fixedNumberEmojiDefs
 });
 
 module.exports = markdownIt;
