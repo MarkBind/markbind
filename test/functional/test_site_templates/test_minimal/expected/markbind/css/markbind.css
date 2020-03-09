MarkBind/markbind

commit f215eeedac66e08cbee9f79c0573a566f4480f40

Migrate to bootstrap-vue popovers (#1033)

asset/css/markbind.css
@@ -340,3 +340,32 @@ li.footnote-item:target {
     top: 0;
     width: 3em;
 }
+
+/* hide popover, modal, tooltip content */
+[data-mb-html-for] {
+    display: none;
+}
+
+/* styles for triggers */
+.trigger {
+    text-decoration: underline dotted;
+}
+
+.modal.mb-zoom {
+    -webkit-transform: scale(0.1);
+    -moz-transform: scale(0.1);
+    -ms-transform: scale(0.1);
+    transform: scale(0.1);
+    opacity: 0;
+    -webkit-transition: all 0.3s;
+    -moz-transition: all 0.3s;
+    transition: all 0.3s;
+}
+
+.modal.mb-zoom.show {
+    -webkit-transform: scale(1);
+    -moz-transform: scale(1);
+    -ms-transform: scale(1);
+    transform: scale(1);
+    opacity: 1;
+}

src/lib/markbind/src/parsers/componentParser.js
@@ -42,6 +42,27 @@ function _parseAttributeWithoutOverride(element, attribute, isInline, slotName =
   delete el.attribs[attribute];
 }
 
+/**
+ * Takes an element, looks for direct elements with slots and transforms to avoid Vue parsing.
+ * This is so that we can use bootstrap-vue popovers, tooltips, and modals.
+ * @param element Element to transform
+ */
+function _transformSlottedComponents(element) {
+  element.children.forEach((child) => {
+    const c = child;
+    const slot = c.attribs && c.attribs.slot;
+    if (slot) {
+      // Turns <div slot="content">... into <div data-mb-html-for=content>...
+      c.attribs['data-mb-html-for'] = slot;
+      delete c.attribs.slot;
+    }
+    // similarly, need to transform templates to avoid Vue parsing
+    if (c.name === 'template') {
+      c.name = 'span';
+    }
+  });
+}
+
 /*
  * Panels
  */
@@ -119,29 +140,79 @@ function _assignPanelId(element) {
   }
 }
 
+/*
+ * Triggers
+ *
+ * At "compile time", we can't tell whether a trigger references a modal, popover, or toolip,
+ * since that element might not have been processed yet.
+ *
+ * So, we make every trigger try all 3. It will attempt to open a tooltip, popover, and modal.
+ *
+ * For tooltips and popovers, we call the relevant content getters inside asset/js/setup.js.
+ * They will check to see if the element id exists, and whether it is a popover/tooltip,
+ * and then return the content as needed.
+ *
+ * For modals, we make it attempt to show the modal if it exists.
+ */
+
+function _parseTrigger(element) {
+  const el = element;
+  el.name = 'span';
+  const trigger = el.attribs.trigger || 'hover';
+  const placement = el.attribs.placement || 'top';
+  el.attribs[`v-b-popover.${trigger}.${placement}.html`]
+    = 'popoverGenerator';
+  el.attribs[`v-b-tooltip.${trigger}.${placement}.html`]
+    = 'tooltipContentGetter';
+  const convertedTrigger = trigger === 'hover' ? 'mouseover' : trigger;
+  el.attribs[`v-on:${convertedTrigger}`] = `$refs['${el.attribs.for}'].show()`;
+  el.attribs.class = el.attribs.class ? `${el.attribs.class} trigger` : 'trigger';
+}
 
 /*
  * Popovers
+ *
+ * We hide the content and header via _transformSlottedComponents, for retrieval by triggers.
+ *
+ * Then, we add in a trigger for this popover.
  */
 
-function _parsePopoverAttributes(element) {
-  _parseAttributeWithoutOverride(element, 'content', true);
-  _parseAttributeWithoutOverride(element, 'header', true);
+function _parsePopover(element) {
+  const el = element;
+  _parseAttributeWithoutOverride(el, 'content', true);
+  _parseAttributeWithoutOverride(el, 'header', true);
   // TODO deprecate title attribute for popovers
-  _parseAttributeWithoutOverride(element, 'title', true, 'header');
+  _parseAttributeWithoutOverride(el, 'title', true, 'header');
+
+  el.name = 'span';
+  const trigger = el.attribs.trigger || 'hover';
+  const placement = el.attribs.placement || 'top';
+  el.attribs['data-mb-component-type'] = 'popover';
+  el.attribs[`v-b-popover.${trigger}.${placement}.html`]
+    = 'popoverInnerGenerator';
+  el.attribs.class = el.attribs.class ? `${el.attribs.class} trigger` : 'trigger';
+  _transformSlottedComponents(el);
 }
 
 /*
  * Tooltips
+ *
+ * Similar to popovers.
  */
 
-function _parseTooltipAttributes(element) {
-  _parseAttributeWithoutOverride(element, 'content', true, '_content');
+function _parseTooltip(element) {
+  const el = element;
+  _parseAttributeWithoutOverride(el, 'content', true, '_content');
+
+  el.name = 'span';
+  const trigger = el.attribs.trigger || 'hover';
+  const placement = el.attribs.placement || 'top';
+  el.attribs['data-mb-component-type'] = 'tooltip';
+  el.attribs[`v-b-tooltip.${trigger}.${placement}.html`]
+    = 'tooltipInnerContentGetter';
+  el.attribs.class = el.attribs.class ? `${el.attribs.class} trigger` : 'trigger';
+  _transformSlottedComponents(el);
 }
-
-/*
- * Modals
- */
 
 function _renameSlot(element, originalName, newName) {
   if (element.children) {
@@ -155,14 +226,60 @@ function _renameSlot(element, originalName, newName) {
   }
 }
 
+function _renameAttribute(element, originalAttribute, newAttribute) {
+  const el = element;
+  if (_.has(el.attribs, originalAttribute)) {
+    el.attribs[newAttribute] = el.attribs[originalAttribute];
+    delete el.attribs[originalAttribute];
+  }
+}
+
+/*
+ * Modals
+ *
+ * We are using bootstrap-vue modals, and some of their attributes/slots differ from ours.
+ * So, we will transform from markbind modal syntax into bootstrap-vue modal syntax.
+ */
+
 function _parseModalAttributes(element) {
-  _parseAttributeWithoutOverride(element, 'header', true, '_header');
+  const el = element;
+  _parseAttributeWithoutOverride(el, 'header', true, 'modal-title');
   // TODO deprecate title attribute for modals
-  _parseAttributeWithoutOverride(element, 'title', true, '_header');
+  _parseAttributeWithoutOverride(el, 'title', true, 'modal-title');
 
   // TODO deprecate modal-header, modal-footer attributes for modals
-  _renameSlot(element, 'modal-header', 'header');
-  _renameSlot(element, 'modal-footer', 'footer');
+  _renameSlot(el, 'header', 'modal-header');
+  _renameSlot(el, 'footer', 'modal-footer');
+
+  el.name = 'b-modal';
+
+  _renameAttribute(el, 'ok-text', 'ok-title');
+  _renameAttribute(el, 'center', 'centered');
+
+  el.attribs['ok-only'] = ''; // only show OK button
+
+  if (el.attribs.backdrop === 'false') {
+    el.attribs['no-close-on-backdrop'] = '';
+  }
+  delete el.attribs.backdrop;
+
+  let size = '';
+  if (_.has(el.attribs, 'large')) {
+    size = 'lg';
+    delete el.attribs.large;
+  } else if (_.has(el.attribs, 'small')) {
+    size = 'sm';
+    delete el.attribs.small;
+  }
+  el.attribs.size = size;
+
+  // default for markbind is zoom, default for bootstrap-vue is fade
+  const effect = el.attribs.effect === 'fade' ? '' : 'mb-zoom';
+  el.attribs['modal-class'] = effect;
+
+  if (_.has(el.attribs, 'id')) {
+    el.attribs.ref = el.attribs.id;
+  }
 }
 
 /*
@@ -224,11 +341,14 @@ function parseComponents(element, errorHandler) {
     case 'panel':
       _parsePanelAttributes(element);
       break;
+    case 'trigger':
+      _parseTrigger(element);
+      break;
     case 'popover':
-      _parsePopoverAttributes(element);
+      _parsePopover(element);
       break;
     case 'tooltip':
-      _parseTooltipAttributes(element);
+      _parseTooltip(element);
       break;
     case 'modal':
       _parseModalAttributes(element);

test/functional/test_site/expected/bugs/index.html
@@ -38,44 +38,35 @@
           <p><strong>popover initiated by trigger: honor trigger attribute</strong></p>
           <p><a href="https://github.com/MarkBind/markbind/issues/49">Issue #49</a></p>
           <p>Repro:</p>
-          <p>
-            <trigger for="pop:xp-user-stories">Establishing Requirements</trigger>
-          </p>
-          <popover id="pop:xp-user-stories" trigger="click">
-            <div slot="content">
-              <div>
-                <p>Requirements gathering, requirements elicitation, requirements analysis, requirements capture are some of the terms commonly <strong>and</strong> interchangeably used to represent the activity of understanding what a software product should
-                  do.
-                </p>
-              </div>
-            </div>
-          </popover>
+          <p><span for="pop:xp-user-stories" v-b-popover.hover.top.html="popoverGenerator" v-b-tooltip.hover.top.html="tooltipContentGetter" v-on:mouseover="$refs['pop:xp-user-stories'].show()" class="trigger">Establishing Requirements</span></p>
+          <span id="pop:xp-user-stories" trigger="click" data-mb-component-type="popover" v-b-popover.click.top.html="popoverInnerGenerator" class="trigger">
+  <div data-mb-html-for="content">
+    <div>
+<p>Requirements gathering, requirements elicitation, requirements analysis,
+requirements capture are some of the terms commonly <strong>and</strong> interchangeably used to represent the activity
+of understanding what a software product should do.</p></div></div></span>
           <p><strong>Support multiple inclusions of a modal</strong></p>
           <p><a href="https://github.com/MarkBind/markbind/issues/107">Issue #107</a></p>
           <p>Repro:</p>
           <div>
-            <p>This is to reproduce
-              <trigger trigger="click" for="modal:bugRepro">multiple inclusions of a modal bug</trigger>
-            </p>
-            <modal large="" id="modal:bugRepro"><template slot="_header">Establishing Requirements</template>
+            <p>This is to reproduce <span trigger="click" for="modal:bugRepro" v-b-popover.click.top.html="popoverGenerator" v-b-tooltip.click.top.html="tooltipContentGetter" v-on:click="$refs['modal:bugRepro'].show()" class="trigger">multiple inclusions of a modal bug</span></p>
+            <b-modal id="modal:bugRepro" ok-only="" size="lg" modal-class="mb-zoom" ref="modal:bugRepro"><template slot="modal-title">Establishing Requirements</template>
               <div>
                 <p>Requirements gathering, requirements elicitation, requirements analysis, requirements capture are some of the terms commonly <strong>and</strong> interchangeably used to represent the activity of understanding what a software product should
                   do.
                 </p>
               </div>
-            </modal>
+            </b-modal>
           </div>
           <div>
-            <p>This is to reproduce
-              <trigger trigger="click" for="modal:bugRepro">multiple inclusions of a modal bug</trigger>
-            </p>
-            <modal large="" id="modal:bugRepro"><template slot="_header">Establishing Requirements</template>
+            <p>This is to reproduce <span trigger="click" for="modal:bugRepro" v-b-popover.click.top.html="popoverGenerator" v-b-tooltip.click.top.html="tooltipContentGetter" v-on:click="$refs['modal:bugRepro'].show()" class="trigger">multiple inclusions of a modal bug</span></p>
+            <b-modal id="modal:bugRepro" ok-only="" size="lg" modal-class="mb-zoom" ref="modal:bugRepro"><template slot="modal-title">Establishing Requirements</template>
               <div>
                 <p>Requirements gathering, requirements elicitation, requirements analysis, requirements capture are some of the terms commonly <strong>and</strong> interchangeably used to represent the activity of understanding what a software product should
                   do.
                 </p>
               </div>
-            </modal>
+            </b-modal>
           </div>
           <p><strong>Remove extra space in links</strong></p>
           <p><a href="https://github.com/MarkBind/markbind/issues/147">Issue #147</a></p>

test/functional/test_site/expected/markbind/css/markbind.css
@@ -340,3 +340,32 @@ li.footnote-item:target {
     top: 0;
     width: 3em;
 }
+
+/* hide popover, modal, tooltip content */
+[data-mb-html-for] {
+    display: none;
+}
+
+/* styles for triggers */
+.trigger {
+    text-decoration: underline dotted;
+}
+
+.modal.mb-zoom {
+    -webkit-transform: scale(0.1);
+    -moz-transform: scale(0.1);
+    -ms-transform: scale(0.1);
+    transform: scale(0.1);
+    opacity: 0;
+    -webkit-transition: all 0.3s;
+    -moz-transition: all 0.3s;
+    transition: all 0.3s;
+}
+
+.modal.mb-zoom.show {
+    -webkit-transform: scale(1);
+    -moz-transform: scale(1);
+    -ms-transform: scale(1);
+    transform: scale(1);
+    opacity: 1;
+}

test/functional/test_site_algolia_plugin/expected/markbind/css/markbind.css
@@ -340,3 +340,32 @@ li.footnote-item:target {
     top: 0;
     width: 3em;
 }
+
+/* hide popover, modal, tooltip content */
+[data-mb-html-for] {
+    display: none;
+}
+
+/* styles for triggers */
+.trigger {
+    text-decoration: underline dotted;
+}
+
+.modal.mb-zoom {
+    -webkit-transform: scale(0.1);
+    -moz-transform: scale(0.1);
+    -ms-transform: scale(0.1);
+    transform: scale(0.1);
+    opacity: 0;
+    -webkit-transition: all 0.3s;
+    -moz-transition: all 0.3s;
+    transition: all 0.3s;
+}
+
+.modal.mb-zoom.show {
+    -webkit-transform: scale(1);
+    -moz-transform: scale(1);
+    -ms-transform: scale(1);
+    transform: scale(1);
+    opacity: 1;
+}

test/functional/test_site_convert/expected/markbind/css/markbind.css
@@ -340,3 +340,32 @@ li.footnote-item:target {
     top: 0;
     width: 3em;
 }
+
+/* hide popover, modal, tooltip content */
+[data-mb-html-for] {
+    display: none;
+}
+
+/* styles for triggers */
+.trigger {
+    text-decoration: underline dotted;
+}
+
+.modal.mb-zoom {
+    -webkit-transform: scale(0.1);
+    -moz-transform: scale(0.1);
+    -ms-transform: scale(0.1);
+    transform: scale(0.1);
+    opacity: 0;
+    -webkit-transition: all 0.3s;
+    -moz-transition: all 0.3s;
+    transition: all 0.3s;
+}
+
+.modal.mb-zoom.show {
+    -webkit-transform: scale(1);
+    -moz-transform: scale(1);
+    -ms-transform: scale(1);
+    transform: scale(1);
+    opacity: 1;
+}

test/functional/test_site_expressive_layout/expected/markbind/css/markbind.css
@@ -340,3 +340,32 @@ li.footnote-item:target {
     top: 0;
     width: 3em;
 }
+
+/* hide popover, modal, tooltip content */
+[data-mb-html-for] {
+    display: none;
+}
+
+/* styles for triggers */
+.trigger {
+    text-decoration: underline dotted;
+}
+
+.modal.mb-zoom {
+    -webkit-transform: scale(0.1);
+    -moz-transform: scale(0.1);
+    -ms-transform: scale(0.1);
+    transform: scale(0.1);
+    opacity: 0;
+    -webkit-transition: all 0.3s;
+    -moz-transition: all 0.3s;
+    transition: all 0.3s;
+}
+
+.modal.mb-zoom.show {
+    -webkit-transform: scale(1);
+    -moz-transform: scale(1);
+    -ms-transform: scale(1);
+    transform: scale(1);
+    opacity: 1;
+}

test/functional/test_site_templates/test_default/expected/index.html
@@ -89,12 +89,12 @@
         <p><strong>A <code>code</code> example:</strong></p>
         <pre><code class="hljs html"><span><span class="hljs-tag">&lt;<span class="hljs-name">foo</span>&gt;</span></span><span>  <span class="hljs-tag">&lt;<span class="hljs-name">bar</span> <span class="hljs-attr">type</span>=<span class="hljs-string">"name"</span>&gt;</span>goo<span class="hljs-tag">&lt;/<span class="hljs-name">bar</span>&gt;</span></span><span><span class="hljs-tag">&lt;/<span class="hljs-name">foo</span>&gt;</span></span></code></pre>
         <h2 id="sub-heading-1-1">Sub Heading 1.1<a class="fa fa-anchor" href="#sub-heading-1-1"></a></h2>
-        <p>A
-          <tooltip effect="scale" placement="top" trigger="hover"><template slot="_content">❗️ some <strong>important explanation</strong></template>tooltip</tooltip>, a
-          <trigger for="modal:modalinfo" trigger="click">modal</trigger>, a <a href="https://markbind.org/">link</a>, a <span class="badge badge-danger">badge</span>, another <span class="badge badge-warning">badge</span>.</p>
-        <modal id="modal:modalinfo"><template slot="_header">Modal Title</template> Some text some text some text some text some text some text some text. Some text some text some text some text some text some text some text. Some text some text some text some text some text some
-          text some text some text some text some text some text some text some text some text. Some text some text some text some text some text some text. Some text some text some text some text some text some text some text.
-        </modal>
+        <p>A <span effect="scale" placement="top" trigger="hover" data-mb-component-type="tooltip" v-b-tooltip.hover.top.html="tooltipInnerContentGetter" class="trigger"><span data-mb-html-for="_content">❗️ some <strong>important explanation</strong></span>tooltip</span>,
+          a <span for="modal:modalinfo" trigger="click" v-b-popover.click.top.html="popoverGenerator" v-b-tooltip.click.top.html="tooltipContentGetter" v-on:click="$refs['modal:modalinfo'].show()" class="trigger">modal</span>, a <a href="https://markbind.org/">link</a>,
+          a <span class="badge badge-danger">badge</span>, another <span class="badge badge-warning">badge</span>.</p>
+        <b-modal id="modal:modalinfo" ok-only="" size="" modal-class="mb-zoom" ref="modal:modalinfo"><template slot="modal-title">Modal Title</template> Some text some text some text some text some text some text some text. Some text some text some text some text some text some text some text. Some text some text some text some text some text
+          some text some text some text some text some text some text some text some text some text. Some text some text some text some text some text some text. Some text some text some text some text some text some text some text.
+        </b-modal>
         <p><strong>A table:</strong></p>
         <div class="table-responsive">
           <table class="markbind-table table table-bordered table-striped">

commit b0c2dd9857e153f3ed087f2b9cf10b0df16c301b

Show pointer and use underline dashed for click trigger
A regression from #1033

Let's add back the two styles of triggers:
- click: show mouse pointer to signal that it is clickable
  and use underline dashed
- not click: no special cursor, and use underline dotted

asset/css/markbind.css
@@ -381,6 +381,11 @@ li.footnote-item:target {
     text-decoration: underline dotted;
 }
 
+.trigger-click {
+    cursor: pointer;
+    text-decoration: underline dashed;
+}
+
 .modal.mb-zoom {
     -webkit-transform: scale(0.1);
     -moz-transform: scale(0.1);

src/lib/markbind/src/parsers/componentParser.js
@@ -188,6 +188,11 @@ function _warnDeprecatedAttributes(node, attributeNamePairs) {
  * For modals, we make it attempt to show the modal if it exists.
  */
 
+function addTriggerClass(node, trigger) {
+  const triggerClass = trigger === 'click' ? 'trigger-click' : 'trigger';
+  node.attribs.class = node.attribs.class ? `${node.attribs.class} ${triggerClass}` : triggerClass;
+}
+
 function _parseTrigger(node) {
   node.name = 'span';
   const trigger = node.attribs.trigger || 'hover';
@@ -198,7 +203,7 @@ function _parseTrigger(node) {
     = 'tooltipContentGetter';
   const convertedTrigger = trigger === 'hover' ? 'mouseover' : trigger;
   node.attribs[`v-on:${convertedTrigger}`] = `$refs['${node.attribs.for}'].show()`;
-  node.attribs.class = node.attribs.class ? `${node.attribs.class} trigger` : 'trigger';
+  addTriggerClass(node, convertedTrigger);
 }
 
 /*
@@ -222,7 +227,7 @@ function _parsePopover(node) {
   node.attribs['data-mb-component-type'] = 'popover';
   node.attribs[`v-b-popover.${trigger}.${placement}.html`]
     = 'popoverInnerGenerator';
-  node.attribs.class = node.attribs.class ? `${node.attribs.class} trigger` : 'trigger';
+  addTriggerClass(node, trigger);
   _transformSlottedComponents(node);
 }
 
@@ -267,7 +272,7 @@ function _parseTooltip(node) {
   node.attribs['data-mb-component-type'] = 'tooltip';
   node.attribs[`v-b-tooltip.${trigger}.${placement}.html`]
     = 'tooltipInnerContentGetter';
-  node.attribs.class = node.attribs.class ? `${node.attribs.class} trigger` : 'trigger';
+  addTriggerClass(node, trigger);
   _transformSlottedComponents(node);
 }
 

test/functional/test_site/expected/bugs/index.html
@@ -39,7 +39,7 @@
           <p><a href="https://github.com/MarkBind/markbind/issues/49">Issue #49</a></p>
           <p>Repro:</p>
           <p><span for="pop:xp-user-stories" v-b-popover.hover.top.html="popoverGenerator" v-b-tooltip.hover.top.html="tooltipContentGetter" v-on:mouseover="$refs['pop:xp-user-stories'].show()" class="trigger">Establishing Requirements</span></p>
-          <span id="pop:xp-user-stories" trigger="click" data-mb-component-type="popover" v-b-popover.click.top.html="popoverInnerGenerator" class="trigger">
+          <span id="pop:xp-user-stories" trigger="click" data-mb-component-type="popover" v-b-popover.click.top.html="popoverInnerGenerator" class="trigger-click">
   <div data-mb-html-for="content">
     <div>
 <p>Requirements gathering, requirements elicitation, requirements analysis,
@@ -49,7 +49,7 @@ of understanding what a software product should do.</p></div></div></span>
           <p><a href="https://github.com/MarkBind/markbind/issues/107">Issue #107</a></p>
           <p>Repro:</p>
           <div>
-            <p>This is to reproduce <span trigger="click" for="modal:bugRepro" v-b-popover.click.top.html="popoverGenerator" v-b-tooltip.click.top.html="tooltipContentGetter" v-on:click="$refs['modal:bugRepro'].show()" class="trigger">multiple inclusions of a modal bug</span></p>
+            <p>This is to reproduce <span trigger="click" for="modal:bugRepro" v-b-popover.click.top.html="popoverGenerator" v-b-tooltip.click.top.html="tooltipContentGetter" v-on:click="$refs['modal:bugRepro'].show()" class="trigger-click">multiple inclusions of a modal bug</span></p>
             <b-modal id="modal:bugRepro" ok-only="" size="lg" modal-class="mb-zoom" ref="modal:bugRepro"><template slot="modal-title">Establishing Requirements</template>
               <div>
                 <p>Requirements gathering, requirements elicitation, requirements analysis, requirements capture are some of the terms commonly <strong>and</strong> interchangeably used to represent the activity of understanding what a software product should
@@ -59,7 +59,7 @@ of understanding what a software product should do.</p></div></div></span>
             </b-modal>
           </div>
           <div>
-            <p>This is to reproduce <span trigger="click" for="modal:bugRepro" v-b-popover.click.top.html="popoverGenerator" v-b-tooltip.click.top.html="tooltipContentGetter" v-on:click="$refs['modal:bugRepro'].show()" class="trigger">multiple inclusions of a modal bug</span></p>
+            <p>This is to reproduce <span trigger="click" for="modal:bugRepro" v-b-popover.click.top.html="popoverGenerator" v-b-tooltip.click.top.html="tooltipContentGetter" v-on:click="$refs['modal:bugRepro'].show()" class="trigger-click">multiple inclusions of a modal bug</span></p>
             <b-modal id="modal:bugRepro" ok-only="" size="lg" modal-class="mb-zoom" ref="modal:bugRepro"><template slot="modal-title">Establishing Requirements</template>
               <div>
                 <p>Requirements gathering, requirements elicitation, requirements analysis, requirements capture are some of the terms commonly <strong>and</strong> interchangeably used to represent the activity of understanding what a software product should

test/functional/test_site/expected/markbind/css/markbind.css
@@ -381,6 +381,11 @@ li.footnote-item:target {
     text-decoration: underline dotted;
 }
 
+.trigger-click {
+    cursor: pointer;
+    text-decoration: underline dashed;
+}
+
 .modal.mb-zoom {
     -webkit-transform: scale(0.1);
     -moz-transform: scale(0.1);

test/functional/test_site_algolia_plugin/expected/markbind/css/markbind.css
@@ -381,6 +381,11 @@ li.footnote-item:target {
     text-decoration: underline dotted;
 }
 
+.trigger-click {
+    cursor: pointer;
+    text-decoration: underline dashed;
+}
+
 .modal.mb-zoom {
     -webkit-transform: scale(0.1);
     -moz-transform: scale(0.1);

test/functional/test_site_convert/expected/markbind/css/markbind.css
@@ -381,6 +381,11 @@ li.footnote-item:target {
     text-decoration: underline dotted;
 }
 
+.trigger-click {
+    cursor: pointer;
+    text-decoration: underline dashed;
+}
+
 .modal.mb-zoom {
     -webkit-transform: scale(0.1);
     -moz-transform: scale(0.1);

test/functional/test_site_expressive_layout/expected/markbind/css/markbind.css
@@ -381,6 +381,11 @@ li.footnote-item:target {
     text-decoration: underline dotted;
 }
 
+.trigger-click {
+    cursor: pointer;
+    text-decoration: underline dashed;
+}
+
 .modal.mb-zoom {
     -webkit-transform: scale(0.1);
     -moz-transform: scale(0.1);

test/functional/test_site_special_tags/expected/markbind/css/markbind.css
@@ -381,6 +381,11 @@ li.footnote-item:target {
     text-decoration: underline dotted;
 }
 
+.trigger-click {
+    cursor: pointer;
+    text-decoration: underline dashed;
+}
+
 .modal.mb-zoom {
     -webkit-transform: scale(0.1);
     -moz-transform: scale(0.1);

test/functional/test_site_templates/test_default/expected/index.html
@@ -90,7 +90,7 @@
         <pre><code class="hljs html"><span><span class="hljs-tag">&lt;<span class="hljs-name">foo</span>&gt;</span></span><span>  <span class="hljs-tag">&lt;<span class="hljs-name">bar</span> <span class="hljs-attr">type</span>=<span class="hljs-string">"name"</span>&gt;</span>goo<span class="hljs-tag">&lt;/<span class="hljs-name">bar</span>&gt;</span></span><span><span class="hljs-tag">&lt;/<span class="hljs-name">foo</span>&gt;</span></span></code></pre>
         <h2 id="sub-heading-1-1">Sub Heading 1.1<a class="fa fa-anchor" href="#sub-heading-1-1"></a></h2>
         <p>A <span effect="scale" placement="top" trigger="hover" data-mb-component-type="tooltip" v-b-tooltip.hover.top.html="tooltipInnerContentGetter" class="trigger"><span data-mb-html-for="_content">❗️ some <strong>important explanation</strong></span>tooltip</span>,
-          a <span for="modal:modalinfo" trigger="click" v-b-popover.click.top.html="popoverGenerator" v-b-tooltip.click.top.html="tooltipContentGetter" v-on:click="$refs['modal:modalinfo'].show()" class="trigger">modal</span>, a <a href="https://markbind.org/">link</a>,
+          a <span for="modal:modalinfo" trigger="click" v-b-popover.click.top.html="popoverGenerator" v-b-tooltip.click.top.html="tooltipContentGetter" v-on:click="$refs['modal:modalinfo'].show()" class="trigger-click">modal</span>, a <a href="https://markbind.org/">link</a>,
           a <span class="badge badge-danger">badge</span>, another <span class="badge badge-warning">badge</span>.</p>
         <b-modal id="modal:modalinfo" ok-only="" size="" modal-class="mb-zoom" ref="modal:modalinfo"><template slot="modal-title">Modal Title</template> Some text some text some text some text some text some text some text. Some text some text some text some text some text some text some text. Some text some text some text some text some text
           some text some text some text some text some text some text some text some text some text. Some text some text some text some text some text some text. Some text some text some text some text some text some text some text.

test/functional/test_site_templates/test_default/expected/markbind/css/markbind.css
@@ -381,6 +381,11 @@ li.footnote-item:target {
     text-decoration: underline dotted;
 }
 
+.trigger-click {
+    cursor: pointer;
+    text-decoration: underline dashed;
+}
+
 .modal.mb-zoom {
     -webkit-transform: scale(0.1);
     -moz-transform: scale(0.1);

test/functional/test_site_templates/test_minimal/expected/markbind/css/markbind.css
@@ -381,6 +381,11 @@ li.footnote-item:target {
     text-decoration: underline dotted;
 }
 
+.trigger-click {
+    cursor: pointer;
+    text-decoration: underline dashed;
+}
+
 .modal.mb-zoom {
     -webkit-transform: scale(0.1);
     -moz-transform: scale(0.1);