dab3a28e90a0038d7090e13bae1408d9c0a07909
Tan Yuanhong <leotan.yh@gmail.com>
2020-03-14 14:54:37 +0800

    Add class support for octicons (#1122)

--- src/lib/markbind/src/lib/markdown-it/markdown-it-icons.js
+++ src/lib/markbind/src/lib/markdown-it/markdown-it-icons.js
@@ -1,15 +1,20 @@
 const octicons = require('@primer/octicons');
 
 module.exports = require('markdown-it-regexp')(
-    /:(fa[brs]|glyphicon|octicon)-([a-z-]+):/,
-    (match, utils) => {
+    /:(fa[brs]|glyphicon|octicon|octiconlight)-([a-z-]+)~?([a-z-]+)?:/,
+    (match, _) => {
         let iconFontType = match[1];
         let iconFontName = match[2];
+        let iconClass = match[3];
 
         if (iconFontType === 'glyphicon') {
             return `<span aria-hidden="true" class="glyphicon glyphicon-${iconFontName}"></span>`;
         } else if (iconFontType === 'octicon') {
-            return octicons[iconFontName].toSVG();
+            return iconClass ? octicons[iconFontName].toSVG({"class": iconClass})
+                             : octicons[iconFontName].toSVG();
+        } else if (iconFontType === 'octiconlight') {
+            return iconClass ? octicons[iconFontName].toSVG({"style": "color: #fff;", "class": iconClass})
+                             : octicons[iconFontName].toSVG({"style": "color: #fff;"});
         } else { // If icon is a Font Awesome icon
             return `<span aria-hidden="true" class="${iconFontType} fa-${iconFontName}"></span>`;
         }
