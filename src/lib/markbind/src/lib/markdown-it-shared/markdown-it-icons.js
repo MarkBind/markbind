ac60496f66e7db1e21ccdb9c323cfcaca5394156
Tan Yuanhong <leotan.yh@gmail.com>
2020-03-14 14:54:37 +0800

    Add class support for octicons

--- src/lib/markbind/src/lib/markdown-it-shared/markdown-it-icons.js
+++ src/lib/markbind/src/lib/markdown-it-shared/markdown-it-icons.js
@@ -1,15 +1,20 @@
 const octicons = require('@primer/octicons');
 
 module.exports = require('markdown-it-regexp')(
-    /:(fa[brs]|glyphicon|octicon)-([a-z-]+):/,
+    /:(fa[brs]|glyphicon|octicon)-([a-z-]+)~?([a-z-]+)?:/,
     (match, utils) => {
         let iconFontType = match[1];
         let iconFontName = match[2];
+        let iconClass = match[3];
 
         if (iconFontType === 'glyphicon') {
             return `<span aria-hidden="true" class="glyphicon glyphicon-${iconFontName}"></span>`;
         } else if (iconFontType === 'octicon') {
+          if (iconClass) {
+            return octicons[iconFontName].toSVG({"class": iconClass});
+          } else {
             return octicons[iconFontName].toSVG();
+          }
         } else { // If icon is a Font Awesome icon
             return `<span aria-hidden="true" class="${iconFontType} fa-${iconFontName}"></span>`;
         }
