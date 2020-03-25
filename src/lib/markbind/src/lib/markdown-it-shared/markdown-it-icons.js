f40e0e500ea3133be67e129a23dd70f062380573
Tan Yuanhong <leotan.yh@gmail.com>
2020-03-25 22:42:58 +0800

    Add octiconlight option

--- src/lib/markbind/src/lib/markdown-it-shared/markdown-it-icons.js
+++ src/lib/markbind/src/lib/markdown-it-shared/markdown-it-icons.js
@@ -1,7 +1,7 @@
 const octicons = require('@primer/octicons');
 
 module.exports = require('markdown-it-regexp')(
-    /:(fa[brs]|glyphicon|octicon)-([a-z-]+)~?([a-z-]+)?:/,
+    /:(fa[brs]|glyphicon|octicon|octiconlight)-([a-z-]+)~?([a-z-]+)?:/,
     (match, utils) => {
         let iconFontType = match[1];
         let iconFontName = match[2];
@@ -15,6 +15,8 @@ module.exports = require('markdown-it-regexp')(
           } else {
             return octicons[iconFontName].toSVG();
           }
+        } else if (iconFontType === 'octiconlight') {
+          return octicons[iconFontName].toSVG({"style": "color: #fff;"});
         } else { // If icon is a Font Awesome icon
             return `<span aria-hidden="true" class="${iconFontType} fa-${iconFontName}"></span>`;
         }
