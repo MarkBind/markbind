a8c0be2a39320629130a744f2e9cf393e21c158c
Tan Yuanhong <leotan.yh@gmail.com>
2020-03-28 20:43:05 +0800

    Resolve comments

--- src/lib/markbind/src/lib/markdown-it-shared/markdown-it-icons.js
+++ src/lib/markbind/src/lib/markdown-it-shared/markdown-it-icons.js
@@ -1,7 +1,7 @@
 const octicons = require('@primer/octicons');
 
 module.exports = require('markdown-it-regexp')(
-    /:(fa[brs]|glyphicon|octicon|octiconlight)-([a-z-]+)~?([a-z-]+)?:/,
+    /:(fa[brs]|glyphicon|octicon|octiconlight)-([a-z-]+)(~[a-z-]+)?:/,
     (match, utils) => {
         let iconFontType = match[1];
         let iconFontName = match[2];
@@ -10,13 +10,11 @@ module.exports = require('markdown-it-regexp')(
         if (iconFontType === 'glyphicon') {
             return `<span aria-hidden="true" class="glyphicon glyphicon-${iconFontName}"></span>`;
         } else if (iconFontType === 'octicon') {
-          if (iconClass) {
-            return octicons[iconFontName].toSVG({"class": iconClass});
-          } else {
-            return octicons[iconFontName].toSVG();
-          }
+            return iconClass ? octicons[iconFontName].toSVG({"class": iconClass})
+                             : octicons[iconFontName].toSVG();
         } else if (iconFontType === 'octiconlight') {
-          return octicons[iconFontName].toSVG({"style": "color: #fff;"});
+            return iconClass ? octicons[iconFontName].toSVG({"style": "color: #fff;", "class": iconClass})
+                             : octicons[iconFontName].toSVG({"style": "color: #fff;"});
         } else { // If icon is a Font Awesome icon
             return `<span aria-hidden="true" class="${iconFontType} fa-${iconFontName}"></span>`;
         }
