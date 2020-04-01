ab059ece4019ee26a8dc57f02bb38f3c70aa3ac8
Phang Chun Rong <crphang@gmail.com>
2020-04-02 00:04:30 +0800

    Add inline puml support (#1100)

--- src/plugins/default/markbind-plugin-plantuml.js
+++ src/plugins/default/markbind-plugin-plantuml.js
@@ -6,42 +6,35 @@
 const cheerio = module.parent.require('cheerio');
 const fs = require('fs');
 const path = require('path');
+
 const { exec } = require('child_process');
 const logger = require('../../util/logger');
+const pluginUtil = require('../../util/pluginUtil');
 
 const JAR_PATH = path.resolve(__dirname, 'plantuml.jar');
 
 const {
   ERR_PROCESSING,
-  ERR_READING,
 } = require('../../constants');
 
 // Tracks diagrams that have already been processed
 const processedDiagrams = new Set();
 
 /**
- * Generates diagram and replaces src attribute of puml tag
- * @param src src attribute of the puml tag
- * @param cwf original file that contains the puml tag, we resolve relative src to this file
+ * Generates diagram and returns the file name of the diagram
+ * @param fileName name of the file to be generated
+ * @param content puml dsl used to generate the puml diagram
  * @param config sourcePath and resultPath from parser context
- * @returns {string} resolved src attribute
+ * @returns {string} file name of diagram
  */
-function generateDiagram(src, cwf, config) {
-  const { sourcePath, resultPath } = config;
-  const _cwf = cwf || sourcePath;
+function generateDiagram(fileName, content, config) {
+  const { resultPath } = config;
 
-  // For replacing img.src
-  const diagramSrc = src.replace('.puml', '.png');
+  const outputDir = path.join(path.dirname(resultPath), path.dirname(fileName));
   // Path of the .puml file
-  const rawDiagramPath = path.resolve(path.dirname(_cwf), src);
-  // Path of the .png to be generated
-  const outputFilePath = path.resolve(resultPath, path.relative(sourcePath, rawDiagramPath)
-    .replace('.puml', '.png'));
-  // Output dir for the png file
-  const outputDir = path.dirname(outputFilePath);
-
+  const outputFilePath = path.join(outputDir, path.basename(fileName));
   // Tracks built files to avoid accessing twice
-  if (processedDiagrams.has(outputFilePath)) { return diagramSrc; }
+  if (processedDiagrams.has(outputFilePath)) { return fileName; }
   processedDiagrams.add(outputFilePath);
 
   // Creates output dir if it doesn't exist
@@ -49,49 +42,37 @@ function generateDiagram(src, cwf, config) {
     fs.mkdirSync(outputDir, { recursive: true });
   }
 
-  // Read diagram file, launch PlantUML jar
-  fs.readFile(rawDiagramPath, (err, data) => {
-    if (err) {
-      logger.debug(err);
-      logger.error(`${ERR_READING} ${rawDiagramPath}`);
-      return;
-    }
+  // Java command to launch PlantUML jar
+  const cmd = `java -jar "${JAR_PATH}" -pipe > "${outputFilePath}"`;
+  const childProcess = exec(cmd);
 
-    const umlCode = data.toString();
+  let errorLog = '';
+  childProcess.stdin.write(
+    content,
+    (e) => {
+      if (e) {
+        logger.debug(e);
+        logger.error(`${ERR_PROCESSING} ${fileName}`);
+      }
+      childProcess.stdin.end();
+    },
+  );
 
-    // Java command to launch PlantUML jar
-    const cmd = `java -jar "${JAR_PATH}" -pipe > "${outputFilePath}"`;
-    const childProcess = exec(cmd);
-
-    let errorLog = '';
-
-    childProcess.stdin.write(
-      umlCode,
-      (e) => {
-        if (e) {
-          logger.debug(e);
-          logger.error(`${ERR_PROCESSING} ${rawDiagramPath}`);
-        }
-        childProcess.stdin.end();
-      },
-    );
-
-    childProcess.on('error', (error) => {
-      logger.debug(error);
-      logger.error(`${ERR_PROCESSING} ${rawDiagramPath}`);
-    });
-
-    childProcess.stderr.on('data', (errorMsg) => {
-      errorLog += errorMsg;
-    });
-
-    childProcess.on('exit', () => {
-      // This goes to the log file, but not shown on the console
-      logger.debug(errorLog);
-    });
+  childProcess.on('error', (error) => {
+    logger.debug(error);
+    logger.error(`${ERR_PROCESSING} ${fileName}`);
   });
 
-  return diagramSrc;
+  childProcess.stderr.on('data', (errorMsg) => {
+    errorLog += errorMsg;
+  });
+
+  childProcess.on('exit', () => {
+    // This goes to the log file, but not shown on the console
+    logger.debug(errorLog);
+  });
+
+  return fileName;
 }
 
 module.exports = {
@@ -101,11 +82,14 @@ module.exports = {
     // Processes all <puml> tags
     const $ = cheerio.load(content, { xmlMode: true });
     $('puml').each((i, tag) => {
-      // eslint-disable-next-line no-param-reassign
       tag.name = 'pic';
-      const { src, cwf } = tag.attribs;
-      // eslint-disable-next-line no-param-reassign
-      tag.attribs.src = generateDiagram(src, cwf, config);
+      const { cwf } = tag.attribs;
+      const pumlContent = pluginUtil.getPluginContent($, tag, cwf);
+
+      const filePath = pluginUtil.getFilePathForPlugin(tag.attribs, pumlContent);
+
+      tag.attribs.src = generateDiagram(filePath, pumlContent, config);
+      tag.children = [];
     });
 
     return $.html();
@@ -113,4 +97,6 @@ module.exports = {
   getSources: () => ({
     tagMap: [['puml', 'src']],
   }),
+
+  getSpecialTags: () => ['puml'],
 };
