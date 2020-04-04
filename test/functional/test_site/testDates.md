50508bd553c44d02f334fdc9a6d0d24531d2fad6
alyip98 <alyip98@gmail.com>
2020-04-04 20:41:05 +0800

    Test: Add tests for date filter

---
+++ test/functional/test_site/testDates.md
@@ -0,0 +1,26 @@
+## Dates
+<variable name="base1">2019-08-12</variable>
+
+{% set format1 = "DD MM YYYY" %}
+
+{% set format2 = "ddd DD/MM" %} 
+
+
+{{ base1 | date }} should be Mon 12 Aug
+
+{{ base1 | date(format1) }} should be 12 08 2019
+
+{{ base1 | date(format1, 10) }} should be 22 08 2019
+
+{{ base1 | date(format2, 10) }} should be Thu 22/08
+
+
+{{ base2 | date }} should be Tue 1 Jan
+
+{{ base2 | date(format1) }} should be 01 01 2019
+
+{{ base2 | date(format1, 10) }} should be 11 01 2019
+
+{{ base2 | date(format2, 10) }} should be Fri 11/01
+
+{{ formatted_date }} should be Fri 11 Jan
--- test/unit/nunjuckUtils.test.js
+++ test/unit/nunjuckUtils.test.js
@@ -1,23 +1,22 @@
-const nunjucks = require('nunjucks');
-const nunjuckUtils = require('../../src/lib/markbind/src/utils/nunjuckUtils');
+const njUtil = require('../../src/lib/markbind/src/utils/nunjuckUtils');
 
 test('Escaping nunjucks raw tags', () => {
   const escapedString = 'This is a content with escaped data {%raw%} CONTENT {%endraw%}';
-  const escapedContent = nunjuckUtils.renderEscaped(nunjucks, escapedString);
+  const escapedContent = njUtil.renderRaw(escapedString);
 
   expect(escapedContent).toBe(escapedString);
 });
 
 test('Escaping nunjucks with new lines', () => {
   const escapedString = 'This is a content with escaped data\n {%raw%} \nCONTENT\n {%endraw%}';
-  const escapedContent = nunjuckUtils.renderEscaped(nunjucks, escapedString);
+  const escapedContent = njUtil.renderRaw(escapedString);
 
   expect(escapedContent).toBe(escapedString);
 });
 
 test('Escaping multiple nunjucks raw tags', () => {
   const escapedString = 'Multiple escapes: {%raw%} first {%endraw%} {%raw%} second {%endraw%}';
-  const escapedContent = nunjuckUtils.renderEscaped(nunjucks, escapedString);
+  const escapedContent = njUtil.renderRaw(escapedString);
 
   expect(escapedContent).toBe(escapedString);
 });
