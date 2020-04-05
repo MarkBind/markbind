1519ad183ad05a4449747c3bf4ff633e0b5e758d
Ang Ze Yu <angzeyu@gmail.com>
2020-04-06 01:09:17 +0800

    Expand test extensions and fix whitespace checks (#1156)
    The diffing utilities used for functional tests only check for
    differences in html files and the siteData json file.
    Additionally, the diffs are whitespace and newline insensitive, which
    can cause subtle, unintended changes to go unnoticed.
    
    Let's expand the range of files to diff, using the isTextOrBinary
    package as a first guard against diffing binary files.
    Let's add additional constants that guard against binary files not
    recognised by the package, or misrecognised as such.
    
    Let's also change the diffing function used to be whitespace and
    newline sensitive.
    Since whitespaces and newlines now appear in the diffs, let's also
    improve the diff printing utilities to account for it.

---
+++ test/functional/testUtil/diffChars.js
@@ -0,0 +1,23 @@
+const jsdiff = require('diff');
+const DiffPrinter = require('./diffPrinter');
+
+/**
+ * Checks for any diffs between expected.html and actual.html,
+ * then prints the differences.
+ * @param {string} expected
+ * @param {string} actual
+ * @param {string} filePathName
+ * @returns {boolean} if diff was found
+ */
+const diffCharsAndPrint = (expected, actual, filePathName) => {
+  const diffParts = jsdiff.diffChars(expected, actual);
+  const isDiff = part => part.added || part.removed;
+  const hasDiff = diffParts.some(isDiff);
+  if (hasDiff) {
+    DiffPrinter.printDiffFoundMessage(filePathName);
+    DiffPrinter.printDiff(diffParts);
+  }
+  return hasDiff;
+};
+
+module.exports = diffCharsAndPrint;
