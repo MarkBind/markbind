8f2fe1142ab977cd6ae7023fd910a98872fd6fc6
Ze Yu <angzeyu@gmail.com>
2020-05-28 03:18:49 +0800

    Rewrite anchor plugin to use new plugin hooks

--- src/plugins/default/markbind-plugin-anchors.js
+++ src/plugins/default/markbind-plugin-anchors.js
@@ -2,19 +2,20 @@ const cheerio = module.parent.require('cheerio');
 
 const CSS_FILE_NAME = 'markbind-plugin-anchors.css';
 
+const HEADING_PATTERN = new RegExp('^h[1-6]$');
+
 /**
  * Adds anchor links to headers
  */
 module.exports = {
   getLinks: (content, pluginContext, frontMatter, utils) => [utils.buildStylesheet(CSS_FILE_NAME)],
-  postRender: (content) => {
-    const $ = cheerio.load(content, { xmlMode: false });
-    $(':header').each((i, heading) => {
-      if ($(heading).attr('id')) {
-        $(heading).append(
-          `<a class="fa fa-anchor" href="#${$(heading).attr('id')}" onclick="event.stopPropagation()"></a>`);
-      }
-    });
-    return $.html();
+  postRenderNode: (node) => {
+    if (HEADING_PATTERN.test(node.name) && node.attribs.id) {
+      const anchorElement = cheerio.parseHTML(
+        `<a class="fa fa-anchor" href="#${node.attribs.id}" onclick="event.stopPropagation()"></a>`,
+      );
+      node.children = [...node.children, ...anchorElement];
+    }
+    return node;
   },
 };
