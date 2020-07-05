93bed7643083bf6104a5f8badfbb117a999d8cbf
Ze Yu <angzeyu@gmail.com>
2020-07-05 20:25:56 +0800

    Bump eslint major version

--- packages/vue-components/webpack.dev.js
+++ packages/vue-components/webpack.dev.js
@@ -4,7 +4,6 @@ const merge = require('webpack-merge');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const config = require('./webpack.common.js');
 
-
 module.exports = merge(config, {
   mode: 'development',
   output: {
