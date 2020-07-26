92444c5051579bf4d1af6f952afe432b862786c4
Ze Yu <angzeyu@gmail.com>
2020-07-26 14:59:16 +0800

    Upgrade vue-components devDependencies

--- packages/vue-components/webpack.build.js
+++ packages/vue-components/webpack.build.js
@@ -1,5 +1,5 @@
 const webpack = require('webpack');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 
 const TerserPlugin = require('terser-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
--- packages/vue-components/webpack.dev.js
+++ packages/vue-components/webpack.dev.js
@@ -1,5 +1,5 @@
 const webpack = require('webpack');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const config = require('./webpack.common.js');
