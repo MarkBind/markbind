62c91cbfae08f19dc49e8c200af8e7de1afec5f9
wxwxwxwx9 <37920115+wxwxwxwx9@users.noreply.github.com>
2020-09-06 18:51:02 +0800

    Move scrollTopButton code into individual js file

--- packages/core-web/src/index.js
+++ packages/core-web/src/index.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import MarkBindVue from '@markbind/vue-components/src';
+import initScrollTopButton from './scrollTopButton';
 import './styles/index.css';
 
 /* global Vue, window, document, jQuery, baseUrl */
@@ -89,55 +90,6 @@ window.handleSiteNavClick = function (elem, useAnchor = true) {
   dropdownIcon.classList.toggle('site-nav-rotate-icon');
 };
 
-window.handleScrollTop = function () {
-  document.body.scrollIntoView({ block: 'start', behavior: 'smooth' });
-};
-
-function lightenScrollTopButton(scrollTopButton, timers) {
-  // lightens the scroll-top-button after 1 second of button inactivity
-  // prevents the button from obscuring the content
-  timers.lightenButtonTimer = setTimeout(() => {
-    if (!scrollTopButton.classList.contains('lighten')) {
-      scrollTopButton.classList.add('lighten');
-    }
-  }, 1000);
-}
-
-function showOrHideScrollTopButton(scrollTopButton, timers) {
-  timers.showOrHideButtonTimer = setTimeout(() => {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      scrollTopButton.style.display = 'block';
-      lightenScrollTopButton(scrollTopButton, timers);
-    } else {
-      scrollTopButton.style.display = 'none';
-    }
-  }, 100);
-}
-
-function resetScrollTopButton(scrollTopButton, timers) {
-  clearTimeout(timers.displayButtonTimer);
-  clearTimeout(timers.lightenButtonTimer);
-  scrollTopButton.classList.remove('lighten');
-}
-
-function promptScrollTopButton(timers) {
-  const scrollTopButton = document.querySelector('#scroll-top-button');
-  resetScrollTopButton(scrollTopButton, timers);
-  showOrHideScrollTopButton(scrollTopButton, timers);
-}
-
-function initScrollTopButton() {
-  const timers = {
-    showOrHideButtonTimer: 0,
-    lightenButtonTimer: 0,
-  };
-  window.addEventListener('scroll', () => {
-    promptScrollTopButton(timers);
-  });
-}
-
-initScrollTopButton();
-
 function setup() {
   // eslint-disable-next-line no-unused-vars
   const vm = new Vue({
@@ -207,5 +159,7 @@ window.popoverInnerGenerator = { title: popoverInnerHeaderGetter, content: popov
 
 window.tooltipContentGetter = makeHtmlGetterFor('tooltip', '_content');
 window.tooltipInnerContentGetter = makeInnerGetterFor('_content');
+
+initScrollTopButton();
 
 export default { setup, setupWithSearch };
