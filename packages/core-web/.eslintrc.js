5ac581cbc8846278679ab8d56bb9b2b151d97c8b
wxwxwxwx9 <37920115+wxwxwxwx9@users.noreply.github.com>
2020-09-06 23:28:11 +0800

    Update .eslintrc.js

--- packages/core-web/.eslintrc.js
+++ packages/core-web/.eslintrc.js
@@ -1,25 +1,14 @@
 module.exports = {
-  'extends': [
-    '../../.eslintrc.js',
-    'plugin:vue/recommended',
-  ],
+  'extends': ['../../.eslintrc.js'],
   'globals': {
     'jQuery': 'readonly',
     'Vue': 'readonly',
     'baseUrl': 'readonly',
   },
+  'env': {
+    'browser': true,
+  },
   'rules': {
     'quote-props': 'off',
-    'vue/html-self-closing': [
-      'error',
-      {
-        'html': {
-          'void': 'always',
-          'normal': 'never',
-        },
-      },
-    ],
-    'vue/max-attributes-per-line': ['error', { 'singleline': 2 }],
-    'vue/order-in-components': 'off',
   },
 };
