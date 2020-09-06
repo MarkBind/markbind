d419521be4c75b7eb7fce0991d8cc11051e38659
wxwxwxwx9 <37920115+wxwxwxwx9@users.noreply.github.com>
2020-09-06 20:42:52 +0800

    Add eslint file for core-web

---
+++ packages/core-web/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  'extends': [
+    '../../.eslintrc.js',
+    'plugin:vue/recommended',
+  ],
+  'globals': {
+    'jQuery': 'readonly',
+    'Vue': 'readonly',
+  },
+  'rules': {
+    'quote-props': 'off',
+    'vue/html-self-closing': [
+      'error',
+      {
+        'html': {
+          'void': 'always',
+          'normal': 'never',
+        },
+      },
+    ],
+    'vue/max-attributes-per-line': ['error', { 'singleline': 2 }],
+    'vue/order-in-components': 'off',
+  },
+};
