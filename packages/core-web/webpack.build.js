65457b4fc996b1413a0c9d0ece4ef04f60b77930
wxwxwxwx9 <37920115+wxwxwxwx9@users.noreply.github.com>
2020-09-07 22:16:12 +0800

    disable eslint in webpack config files

--- packages/core-web/webpack.build.js
+++ packages/core-web/webpack.build.js
@@ -6,6 +6,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const config = require('./webpack.common.js');
 
+/* eslint-disable-next-line */
 module.exports = merge(config, {
   mode: 'production',
   module: {
