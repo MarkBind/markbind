3f1fca2d7191db2c9915659b91b5d5e35fdb31f3
Kendrick Ang <kendrick.wh@outlook.com>
2020-09-12 17:50:23 +0800

    migrate core/test/unit/Site.test.js to fs-extra

--- packages/core/test/unit/Site.test.js
+++ packages/core/test/unit/Site.test.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const fs = require('fs-extra-promise');
+const fs = require('fs-extra');
 const ghpages = require('gh-pages');
 const Site = require('../../src/Site');
 
