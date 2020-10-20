9e48ade7e432262f7d45e9da6286438a59089074
Kendrick Ang <kendrick.wh@outlook.com>
2020-10-20 18:19:54 +0800

    refactor logic based on comments

--- packages/core/src/lib/markdown-it/markdown-it-icons.js
+++ packages/core/src/lib/markdown-it/markdown-it-icons.js
@@ -7,21 +7,26 @@ module.exports = require('markdown-it-regexp')(
         let iconFontName = match[2];
         let iconClass = match[3];
 
-        // ensure octicons exist
-        if (iconFontType === 'octicon' || iconFontType === 'octiconlight') {
-            if (!octicons.hasOwnProperty(iconFontName)) {
-              return `<span aria-hidden="true"></span>`;
-            }
-        }
-
         if (iconFontType === 'glyphicon') {
             return `<span aria-hidden="true" class="glyphicon glyphicon-${iconFontName}"></span>`;
         } else if (iconFontType === 'octicon') {
-            return iconClass ? octicons[iconFontName].toSVG({"class": iconClass})
-                             : octicons[iconFontName].toSVG();
+            let icon = iconClass
+                ? octicons[iconFontName].toSVG({"class": iconClass})
+                : octicons[iconFontName].toSVG();
+            // ensure octicons are valid
+            if (!octicons.hasOwnProperty(iconFontName)) {
+                icon = `<span aria-hidden="true"></span>`;
+            }
+            return icon;
         } else if (iconFontType === 'octiconlight') {
-            return iconClass ? octicons[iconFontName].toSVG({"style": "color: #fff;", "class": iconClass})
-                             : octicons[iconFontName].toSVG({"style": "color: #fff;"});
+            let icon = iconClass
+                ? octicons[iconFontName].toSVG({"style": "color: #fff;", "class": iconClass})
+                : octicons[iconFontName].toSVG({"style": "color: #fff;"});
+            // ensure octicons are valid
+            if (!octicons.hasOwnProperty(iconFontName)) {
+                icon = `<span aria-hidden="true"></span>`;
+            }
+            return icon;
         } else { // If icon is a Font Awesome icon
             return `<span aria-hidden="true" class="${iconFontType} fa-${iconFontName}"></span>`;
         }
