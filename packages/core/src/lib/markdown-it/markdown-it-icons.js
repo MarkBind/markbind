8e2c3538f7b17d6591e128c28adfd8aecbb902c1
Kendrick Ang <kendrick.wh@outlook.com>
2020-10-24 13:49:56 +0800

    Add validity check for octicons (#1377)

--- packages/core/src/lib/markdown-it/markdown-it-icons.js
+++ packages/core/src/lib/markdown-it/markdown-it-icons.js
@@ -10,11 +10,21 @@ module.exports = require('markdown-it-regexp')(
         if (iconFontType === 'glyphicon') {
             return `<span aria-hidden="true" class="glyphicon glyphicon-${iconFontName}"></span>`;
         } else if (iconFontType === 'octicon') {
-            return iconClass ? octicons[iconFontName].toSVG({"class": iconClass})
-                             : octicons[iconFontName].toSVG();
+            // ensure octicons are valid
+            if (!octicons.hasOwnProperty(iconFontName)) {
+                return `<span aria-hidden="true"></span>`;
+            }
+            return iconClass
+                ? octicons[iconFontName].toSVG({"class": iconClass})
+                : octicons[iconFontName].toSVG();
         } else if (iconFontType === 'octiconlight') {
-            return iconClass ? octicons[iconFontName].toSVG({"style": "color: #fff;", "class": iconClass})
-                             : octicons[iconFontName].toSVG({"style": "color: #fff;"});
+            // ensure octicons are valid
+            if (!octicons.hasOwnProperty(iconFontName)) {
+               return `<span aria-hidden="true"></span>`;
+            }
+            return iconClass
+                ? octicons[iconFontName].toSVG({"style": "color: #fff;", "class": iconClass})
+                : octicons[iconFontName].toSVG({"style": "color: #fff;"});
         } else { // If icon is a Font Awesome icon
             return `<span aria-hidden="true" class="${iconFontType} fa-${iconFontName}"></span>`;
         }
