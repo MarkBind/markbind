667da56b861c733196ad3d1740fe299f29061acc
Ryo Armanda <ryoarmanda@rocketmail.com>
2020-11-09 20:12:09 +0800

    Correct logging when there are no assets to be removed (#1383)

--- packages/core/src/Site/index.js
+++ packages/core/src/Site/index.js
@@ -792,8 +792,10 @@ class Site {
     const filesToRemove = fileRelativePaths.map(
       fileRelativePath => path.join(this.outputPath, fileRelativePath));
     const removeFiles = filesToRemove.map(asset => fs.remove(asset));
-    return Promise.all(removeFiles)
-      .then(() => logger.info('Assets removed'));
+    return removeFiles.length === 0
+      ? Promise.resolve('')
+      : Promise.all(removeFiles)
+        .then(() => logger.debug('Assets removed'));
   }
 
   buildAssets() {
