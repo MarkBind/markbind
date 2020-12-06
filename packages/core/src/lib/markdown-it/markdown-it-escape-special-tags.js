089cb7c4d9ab5ae0614fde9ce3a1ed813aaa073b
Ang Ze Yu <angzeyu@gmail.com>
2020-12-06 11:41:21 +0800

    Ignore self-closing special tags for markdown-it (#1408)
    The markdown-it patch for special tags does not blacklist self-closed
    special tags from the starting regex pattern. Thus, the starting regex
    pattern incorrectly matches such a self-closing special tag, and the
    ending regex pattern incorrectly seeks for a corresponding closing
    special tag until EOF.
    
    This is not apparent in the functional tests which have such tags
    (<puml>), because puml tags are processed before markdown.
    
    Let's add a negative lookahead to the starting pattern to blacklist
    such tags.

--- packages/core/src/lib/markdown-it/markdown-it-escape-special-tags.js
+++ packages/core/src/lib/markdown-it/markdown-it-escape-special-tags.js
@@ -12,7 +12,20 @@ function escape_plugin(md, tagsToIgnore) {
   const specialTagsRegex = Array.from(tagsToIgnore)
     .concat(['script|pre|style|variable'])
     .join('|');
-  const startingSpecialTagRegex = new RegExp(`^<(${specialTagsRegex})(?=(\\s|>|$))`, 'i');
+
+  // attr_name, unquoted ... attribute patterns adapted from markdown-it/lib/common/html_re
+  // Construct self-closing negative lookahead for special tags,
+  // because if matched, the endingSpecialTagRegex will roll down to find a non-existent closing tag
+  const attr_name = '[a-zA-Z_:][a-zA-Z0-9:._-]*';
+  const unquoted      = '[^"\'=<>`\\x00-\\x20]+';
+  const single_quoted = "'[^']*'";
+  const double_quoted = '"[^"]*"';
+  const attr_value  = '(?:' + unquoted + '|' + single_quoted + '|' + double_quoted + ')';
+  const attribute   = '(?:\\s+' + attr_name + '(?:\\s*=\\s*' + attr_value + ')?)';
+  const selfClosingNegativeLookahead = `(?!${attribute}*\\s*\/>)`;
+
+  const startingSpecialTagRegex = new RegExp(
+    `^<(${specialTagsRegex})${selfClosingNegativeLookahead}(?=(\\s|>|$))`, 'i');
   const endingSpecialTagRegex = new RegExp(`<\\/(${specialTagsRegex})>`, 'i');
 
   const HTML_SEQUENCES = [
