4dec80a7d4bbe5fc49cf162b29c2601ba3f18556
Ze Yu <angzeyu@gmail.com>
2020-12-10 21:10:59 +0800

    Create External and ExternalManager classes
    Files referenced by the 'src' attribute in panels are generated and
    output as separate files, and dynamically retrieved at runtime in the
    browser.
    
    Such generation processes can be extracted into an 'External'
    abstraction, facilitating future similar processes for other components
    besides panels.
    
    Let's create the External, and ExternalManager classes to consolidate
    such generation logic.

---
+++ packages/core/src/External/External.js
@@ -0,0 +1,72 @@
+const fs = require('fs-extra');
+const path = require('path');
+const htmlBeautify = require('js-beautify').html;
+
+const { PageSources } = require('../Page/PageSources');
+const { NodeProcessor } = require('../html/NodeProcessor');
+
+const fsUtil = require('../utils/fsUtil');
+
+/**
+ * Represents external files (e.g. files referenced by <panel src="...">)
+ * not directly included inside the generated page
+ */
+class External {
+  constructor(externalManager, sourceFilePath) {
+    /**
+     * @type {ExternalManager}
+     */
+    this.externalManager = externalManager;
+
+    /**
+     * @type {string}
+     */
+    this.sourceFilePath = sourceFilePath;
+
+    /**
+     * @type {Set<string>}
+     */
+    this.includedFiles = new Set([sourceFilePath]);
+  }
+
+  /**
+   * Generates the content of this External instance
+   * @param asIfAtFilePath
+   * @param resultPath
+   * @param config
+   * @return {Promise<External>}
+   */
+  async resolveDependency(asIfAtFilePath, resultPath, config) {
+    const fileConfig = {
+      ...config,
+      headerIdMap: {},
+    };
+
+    const { variableProcessor, pluginManager } = config;
+
+    const pageSources = new PageSources();
+    const docId = `ext-${fsUtil.removeExtension(path.basename(asIfAtFilePath))}`;
+    const nodeProcessor = new NodeProcessor(fileConfig, pageSources, variableProcessor, pluginManager, docId);
+
+    const nunjucksProcessed = variableProcessor.renderWithSiteVariables(this.sourceFilePath, pageSources);
+    const mdHtmlProcessed = await nodeProcessor.process(this.sourceFilePath, nunjucksProcessed,
+                                                        asIfAtFilePath);
+    const pluginPostRendered = pluginManager.postRender(nodeProcessor.frontMatter, mdHtmlProcessed);
+
+    const outputContentHTML = config.disableHtmlBeautify
+      ? pluginPostRendered
+      : htmlBeautify(pluginPostRendered, pluginManager.htmlBeautifyOptions);
+
+    await fs.outputFile(resultPath, outputContentHTML);
+
+    pageSources.addAllToSet(this.includedFiles);
+
+    await this.externalManager.generateDependencies(pageSources.getDynamicIncludeSrc(), this.includedFiles);
+
+    return this;
+  }
+}
+
+module.exports = {
+  External,
+};
