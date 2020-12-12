4bfea5ce0f2d31ef884c5e465e4659af24dfe209
daongochieu2810 <daongochieu2810@yahoo.com.vn>
2020-12-12 23:49:59 +0800

    Remove unnecessary diffs

--- packages/cli/test/functional/test_site/expected/_markbind/plugins/testMarkbindPluginScript.js
+++
@@ -1,2 +0,0 @@
-// eslint-disable-next-line no-alert
-alert('External plugin script file loaded!');
--- packages/cli/test/functional/test_site/expected/headFiles/customScriptTop.js
+++
@@ -1,2 +0,0 @@
-// eslint-disable-next-line no-console
-console.info('custom script inserted into top of head successfully!');
