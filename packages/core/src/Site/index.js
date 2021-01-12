591eee831d8950ebd77278148c559af08f4dc610
raysonkoh <rayson.rk.koh@gmail.com>
2021-01-12 19:08:25 +0800

    Add initial logic for parsing json in <variable>

--- packages/core/src/Site/index.js
+++ packages/core/src/Site/index.js
@@ -565,9 +565,17 @@ class Site {
 
       const $ = cheerio.load(content, { decodeEntities: false });
       $('variable,span').each((index, element) => {
-        const name = $(element).attr('name') || $(element).attr('id');
+        const type = $(element).attr('type');
+        if (type === 'json') {
+          const parsedJson = JSON.parse($(element).html());
+          Object.entries(parsedJson).forEach(([key, val]) => {
+            this.variableProcessor.renderAndAddUserDefinedVariable(base, key, val);
+          });
+        } else {
+          const name = $(element).attr('name') || $(element).attr('id');
 
-        this.variableProcessor.renderAndAddUserDefinedVariable(base, name, $(element).html());
+          this.variableProcessor.renderAndAddUserDefinedVariable(base, name, $(element).html());
+        }
       });
     });
   }
