656d57c0cad29b245977291a56372dd463b6897a
wxwxwxwx9 <37920115+wxwxwxwx9@users.noreply.github.com>
2021-02-09 22:31:40 +0800

    Remove markdown-it-ins

--- packages/core/src/lib/markdown-it/index.js
+++ packages/core/src/lib/markdown-it/index.js
@@ -17,7 +17,6 @@ markdownIt.use(createDoubleDelimiterInlineRule('%%', 'dimmed', 'emphasis'))
   .use(createDoubleDelimiterInlineRule('--', 'small', 'large'));
 
 markdownIt.use(require('markdown-it-mark'))
-  .use(require('markdown-it-ins'))
   .use(require('markdown-it-sub'))
   .use(require('markdown-it-sup'))
   .use(require('markdown-it-imsize'), {autofill: false})
