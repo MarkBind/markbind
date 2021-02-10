8f98b4ecfbe59ca03a999724f236f984795445a4
wxwxwxwx9 <37920115+wxwxwxwx9@users.noreply.github.com>
2021-02-10 22:25:12 +0800

    Add licensing note and credit

--- packages/core/src/lib/markdown-it/markdown-it-double-delimiter.js
+++ packages/core/src/lib/markdown-it/markdown-it-double-delimiter.js
@@ -1,5 +1,36 @@
 'use strict';
 
+/*
+ Copyright (c) 2014-2015 Vitaly Puzrin, Alex Kocharin
+
+ Permission is hereby granted, free of charge, to any person
+ obtaining a copy of this software and associated documentation
+ files (the "Software"), to deal in the Software without
+ restriction, including without limitation the rights to use,
+ copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the
+ Software is furnished to do so, subject to the following
+ conditions:
+
+ The above copyright notice and this permission notice shall be
+ included in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/*
+ Markdown-it 12.0.4 source 
+ markdown-it/blob/master/lib/rules_inline/strikethrough.js adapted to fit MarkBind's needs, to:
+ - create new inline rules dynamically for different delimiters 
+ */
+
 /**
  * Creates a new inline rule for markdown-it, specifically for rules that uses a 'double' delimiter.
  * Some examples are ++, --, $$, %%. 
