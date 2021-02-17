c3ba2b5e235501a9f36955fd9036d20e14b0a34a
Ang Ze Yu <angzeyu@gmail.com>
2021-02-17 22:53:19 +0800

    Eslint appropriate lib/markdown-it files (#1481)
    The lib/markdown-it directory in the core package is entirely ignored
    by our eslint rules.
    
    Let's tighten the eslint ignore rules to only ignore adapted source
    files that are largely unchanged from the original source.
    
    This allows easily referencing the original source material if needed.

--- packages/core/src/lib/markdown-it/highlight/HighlightRule.js
+++ packages/core/src/lib/markdown-it/highlight/HighlightRule.js
@@ -7,16 +7,16 @@ class HighlightRule {
      */
     this.ruleComponents = ruleComponents;
   }
-  
+
   static parseRule(ruleString) {
     const components = ruleString.split('-').map(HighlightRuleComponent.parseRuleComponent);
     return new HighlightRule(components);
   }
-  
+
   offsetLines(offset) {
     this.ruleComponents.forEach(comp => comp.offsetLineNumber(offset));
   }
-  
+
   shouldApplyHighlight(lineNumber) {
     const compares = this.ruleComponents.map(comp => comp.compareLine(lineNumber));
     if (this.isLineRange()) {
@@ -24,29 +24,29 @@ class HighlightRule {
       const withinRangeEnd = compares[1] >= 0;
       return withinRangeStart && withinRangeEnd;
     }
-    
+
     const atLineNumber = compares[0] === 0;
     return atLineNumber;
   }
-  
+
   applyHighlight(line) {
     const isLineSlice = this.ruleComponents.length === 1 && this.ruleComponents[0].isSlice;
-    
+
     if (this.isLineRange()) {
       const shouldWholeLine = this.ruleComponents.some(comp => comp.isUnboundedSlice());
       return shouldWholeLine
         ? HighlightRule._highlightWholeLine(line)
         : HighlightRule._highlightTextOnly(line);
     }
-    
+
     if (isLineSlice) {
       // TODO: Implement slice-index based highlighting
       return HighlightRule._highlightWholeLine(line);
     }
-    
+
     return HighlightRule._highlightTextOnly(line);
   }
-  
+
   static _highlightWholeLine(codeStr) {
     return `<span class="highlighted">${codeStr}\n</span>`;
   }
@@ -57,10 +57,10 @@ class HighlightRule {
     const content = codeStr.substr(codeStartIdx);
     return [indents, content];
   }
-  
+
   static _highlightTextOnly(codeStr) {
     const [indents, content] = HighlightRule._splitCodeAndIndentation(codeStr);
-    return `<span>${indents}<span class="highlighted">${content}</span>\n</span>`
+    return `<span>${indents}<span class="highlighted">${content}</span>\n</span>`;
   }
 
   isLineRange() {
@@ -69,5 +69,5 @@ class HighlightRule {
 }
 
 module.exports = {
-  HighlightRule
+  HighlightRule,
 };
