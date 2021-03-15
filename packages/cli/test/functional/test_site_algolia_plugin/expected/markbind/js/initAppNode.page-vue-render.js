fc1401f385950dbefa6c62489c3bb628b2909372
wxwxwxwx9 <37920115+wxwxwxwx9@users.noreply.github.com>
2021-03-15 13:44:44 +0800

    Update test

--- packages/cli/test/functional/test_site_algolia_plugin/expected/markbind/js/initAppNode.page-vue-render.js
+++
@@ -1,7 +0,0 @@
-
-      var body = document.querySelector('body');
-      body.innerHTML = '';
-      var newApp = document.createElement('div');
-      newApp.setAttribute('id', 'app');
-      body.appendChild(newApp); 
-    
