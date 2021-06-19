fab49f2c71059bf421222af01c902b3304ac8084
wxwxwxwx9 <37920115+wxwxwxwx9@users.noreply.github.com>
2021-06-19 17:55:38 +0800

    Add appFactory to Vue initialization for setup

--- packages/core-web/src/index.js
+++ packages/core-web/src/index.js
@@ -172,6 +172,7 @@ function setup() {
       return pageVueRenderFn.call(this, createElement);
     },
     staticRenderFns: pageVueStaticRenderFns,
+    ...appFactory(),
     mounted() {
       executeAfterMountedRoutines();
     },
