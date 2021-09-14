f3e4d810af69bb8ce7ee0263de630beb8008fb74
ALZAHRANI-PC\riyad <riyadh-h@outlook.sa>
2021-09-14 22:26:18 +0800

    Fix code styling

--- packages/core/src/plugins/codeBlockButtonsAssets/codeBlockButtonsContainer.js
+++ packages/core/src/plugins/codeBlockButtonsAssets/codeBlockButtonsContainer.js
@@ -1,5 +1,5 @@
 const cheerio = module.parent.require('cheerio');
-const CONTAINER_HTML = `<div class="function-btn-container"></div>`;
+const CONTAINER_HTML = '<div class="function-btn-container"></div>';
 
 function isFunctionBtnContainer(node) {
   return cheerio(node).hasClass('function-btn-container');
@@ -14,5 +14,5 @@ function doesFunctionBtnContainerExistInNode(node) {
 module.exports = {
   CONTAINER_HTML,
   isFunctionBtnContainer,
-  doesFunctionBtnContainerExistInNode
+  doesFunctionBtnContainerExistInNode,
 };
