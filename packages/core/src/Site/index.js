89db3e9fe7be742ba8310ddaf3555769446481d6
ong6 <junxiongong2@gmail.com>
2022-01-31 01:05:37 +0800

    Fixed backslash error for windows

--- packages/core/src/Site/index.js
+++ packages/core/src/Site/index.js
@@ -436,7 +436,7 @@ class Site {
       .filter(addressablePage => !addressablePage.src.startsWith('_'))
       .forEach((page) => {
         const addressablePagePath = path.join(this.rootPath, page.src);
-        const relativePagePathWithoutExt = fsUtil.removeExtension(
+        const relativePagePathWithoutExt = fsUtil.removeExtensionPosix(
           path.relative(this.rootPath, addressablePagePath));
         const pageName = _.startCase(fsUtil.removeExtension(path.basename(addressablePagePath)));
         const pageUrl = `{{ baseUrl }}/${relativePagePathWithoutExt}.html`;
