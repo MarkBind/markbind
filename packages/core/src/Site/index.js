228d24edabd34d21c9d33366f40519a8510a8a03
ong6 <junxiongong2@gmail.com>
2022-01-31 01:02:08 +0800

    used FSutil method

--- packages/core/src/Site/index.js
+++ packages/core/src/Site/index.js
@@ -436,9 +436,8 @@ class Site {
       .filter(addressablePage => !addressablePage.src.startsWith('_'))
       .forEach((page) => {
         const addressablePagePath = path.join(this.rootPath, page.src);
-        const relativePagePathWithoutExt = fsUtil.removeExtension(
-          path.relative(this.rootPath, addressablePagePath))
-          .replace(/\\/g, '/');
+        const relativePagePathWithoutExt = fsUtil.removeExtensionPosix(
+          path.relative(this.rootPath, addressablePagePath));
         const pageName = _.startCase(fsUtil.removeExtension(path.basename(addressablePagePath)));
         const pageUrl = `{{ baseUrl }}/${relativePagePathWithoutExt}.html`;
         siteNavContent += `* [${pageName}](${pageUrl})\n`;
