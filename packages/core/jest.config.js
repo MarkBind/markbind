285f0578f23b44929aa751740dbf9e6f471f8669
Ryo Armanda <24468110+ryoarmanda@users.noreply.github.com>
2022-04-14 17:20:23 +0800

    Add initial typescript support

---
+++ packages/core/jest.config.js
@@ -0,0 +1,14 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  verbose: true,
+  preset: 'ts-jest/presets/js-with-babel',
+  testEnvironment: 'node',
+  // Disable type-checking for test files until we have fully adapted to TypeScript.
+  // Temporarily remove the below lines if you need to type-check the test files
+  // as you run the tests.
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
+  },
+};
