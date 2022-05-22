579e112733f28e1dab33383ff137d508149e0694
Ryo Armanda <24468110+ryoarmanda@users.noreply.github.com>
2022-05-22 21:59:29 +0800

    Add clean script

---
+++ scripts/clean.js
@@ -0,0 +1,48 @@
+/*
+ * Script for cleaning compiled TypeScript files
+ *
+ * This script is a manual substitute of the `tsc --build --clean`
+ * command which can handle the case where the TypeScript file
+ * that generated its associated compiled files has been removed
+ * prior to cleaning.
+ *
+ * The script assumes that the associated compiled files can be
+ * detected through the presence of sourcemap files (`.js.map`)
+ * instead.
+ */
+
+const fs = require('fs');
+const path = require('path');
+const walkSync = require('walk-sync');
+
+const JS_MAP_RE = /\.js\.map$/;
+const COMPILED_FILES_EXTS = ['.d.ts', '.d.ts.map', '.js', '.js.map'];
+
+const packagesDir = path.join(__dirname, '../packages');
+
+const jsMapFiles = walkSync(packagesDir, {
+  globs: ['**/*.js.map'],
+  ignore: ['**/node_modules'],
+  directories: false,
+  includeBasePath: true,
+});
+
+function removeFile(file) {
+  try {
+    fs.rmSync(file);
+  } catch (err) {
+    // No need to error when file is not found
+    if (err.code === 'ENOENT') {
+      return;
+    }
+
+    throw err;
+  }
+}
+
+jsMapFiles.forEach((jsMapFile) => {
+  COMPILED_FILES_EXTS.forEach((ext) => {
+    const extFile = jsMapFile.replace(JS_MAP_RE, ext);
+    removeFile(extFile);
+  });
+});
