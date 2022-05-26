2f9e255f602f593eaae67f618df8f588d4ee4952
Ryo Armanda <24468110+ryoarmanda@users.noreply.github.com>
2022-05-27 00:38:03 +0800

    Adapt constant files to TypeScript

--- packages/core/src/Site/constants.ts
+++ packages/core/src/Site/constants.ts
@@ -1,23 +1,21 @@
-module.exports = {
-  CONFIG_FOLDER_NAME: '_markbind',
-  HEADING_INDEXING_LEVEL_DEFAULT: 3,
-  SITE_FOLDER_NAME: '_site',
-  TEMP_FOLDER_NAME: '.temp',
-  TEMPLATE_SITE_ASSET_FOLDER_NAME: 'markbind',
+export const CONFIG_FOLDER_NAME = '_markbind';
+export const HEADING_INDEXING_LEVEL_DEFAULT = 3;
+export const SITE_FOLDER_NAME = '_site';
+export const TEMP_FOLDER_NAME = '.temp';
+export const TEMPLATE_SITE_ASSET_FOLDER_NAME = 'markbind';
 
-  ABOUT_MARKDOWN_FILE: 'about.md',
-  FAVICON_DEFAULT_PATH: 'favicon.ico',
-  INDEX_MARKDOWN_FILE: 'index.md',
-  MAX_CONCURRENT_PAGE_GENERATION_PROMISES: 4,
-  PAGE_TEMPLATE_NAME: 'page.njk',
-  SITE_CONFIG_NAME: 'site.json',
-  SITE_DATA_NAME: 'siteData.json',
-  LAYOUT_SITE_FOLDER_NAME: 'layouts',
-  LAZY_LOADING_SITE_FILE_NAME: 'LazyLiveReloadLoadingSite.html',
-  LAZY_LOADING_BUILD_TIME_RECOMMENDATION_LIMIT: 30000,
-  LAZY_LOADING_REBUILD_TIME_RECOMMENDATION_LIMIT: 5000,
-  USER_VARIABLES_PATH: '_markbind/variables.md',
-  WIKI_SITE_NAV_PATH: '_Sidebar.md',
-  WIKI_FOOTER_PATH: '_Footer.md',
-  MARKBIND_WEBSITE_URL: 'https://markbind.org/',
-};
+export const ABOUT_MARKDOWN_FILE = 'about.md';
+export const FAVICON_DEFAULT_PATH = 'favicon.ico';
+export const INDEX_MARKDOWN_FILE = 'index.md';
+export const MAX_CONCURRENT_PAGE_GENERATION_PROMISES = 4;
+export const PAGE_TEMPLATE_NAME = 'page.njk';
+export const SITE_CONFIG_NAME = 'site.json';
+export const SITE_DATA_NAME = 'siteData.json';
+export const LAYOUT_SITE_FOLDER_NAME = 'layouts';
+export const LAZY_LOADING_SITE_FILE_NAME = 'LazyLiveReloadLoadingSite.html';
+export const LAZY_LOADING_BUILD_TIME_RECOMMENDATION_LIMIT = 30000;
+export const LAZY_LOADING_REBUILD_TIME_RECOMMENDATION_LIMIT = 5000;
+export const USER_VARIABLES_PATH = '_markbind/variables.md';
+export const WIKI_SITE_NAV_PATH = '_Sidebar.md';
+export const WIKI_FOOTER_PATH = '_Footer.md';
+export const MARKBIND_WEBSITE_URL = 'https://markbind.org/';
