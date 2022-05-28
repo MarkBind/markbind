90d31a5588bf1fc9185b12df3b5069e1765064d9
tlylt <liu_yongliang@hotmail.com>
2022-05-28 23:24:33 +0800

    Add flag

--- packages/core/src/plugins/mathDelimiters.js
+++ packages/core/src/plugins/mathDelimiters.js
@@ -2,13 +2,18 @@ const katex = require('katex');
 const texmath = require('markdown-it-texmath');
 const md = require('../lib/markdown-it');
 
+let pluginAdded = false;
+
 // see https://github.com/goessner/markdown-it-texmath#features for supported delimiters
 // note that some of them may not work correctly due to conflicting syntax
 function mathDelimiters(pluginContext) {
-  if (!pluginContext.delimiters) {
-    throw new Error('No delimiters defined in the plugin context!');
+  if (!pluginAdded) {
+    if (!pluginContext.delimiters) {
+      throw new Error('No delimiters defined in the plugin context!');
+    }
+    md.use(texmath, { engine: katex, delimiters: pluginContext.delimiters });
+    pluginAdded = true; // only need to add once
   }
-  md.use(texmath, { engine: katex, delimiters: pluginContext.delimiters });
 }
 
 module.exports = {
