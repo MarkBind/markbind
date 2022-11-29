fe207f95f5f72d820919547147e56fc1d667310a
Elton Goh <75515229+EltonGohJH@users.noreply.github.com>
2022-11-29 10:02:54 +0800

    Add template for 404 page (#1955)

---
+++ packages/cli/test/functional/test_site_templates/test_default/expected/404.page-vue-render.js
@@ -0,0 +1,10 @@
+
+    var pageVueRenderFn = function anonymous(
+) {
+with(this){return _m(0)}
+};
+    var pageVueStaticRenderFns = [function anonymous(
+) {
+with(this){return _c('div',{attrs:{"id":"app"}},[_c('div',[_c('p'),_c('div',{staticClass:"text-center"},[_c('p',{staticStyle:{"font-size":"10rem"}},[_v("404")])]),_v(" "),_c('div',{staticClass:"text-center"},[_c('p',{staticStyle:{"font-size":"1.5rem"}},[_v("File not found"),_c('br'),_v("Click "),_c('a',{attrs:{"href":"/"}},[_v("here")]),_v(" to go back to the home page.")])]),_c('p'),_v(" "),_c('i',{staticClass:"fa fa-arrow-circle-up fa-lg d-print-none",attrs:{"id":"scroll-top-button","onclick":"handleScrollTop()","aria-hidden":"true"}})])])}
+}];
+  
