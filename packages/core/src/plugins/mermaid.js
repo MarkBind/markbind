d62809783a7c0e03808dffbb4aa4a3d0f958e9a6
tlylt <liu_yongliang@hotmail.com>
2022-12-10 12:01:02 +0800

    Refactor script

--- packages/core/src/plugins/mermaid.js
+++ packages/core/src/plugins/mermaid.js
@@ -1,7 +1,12 @@
-const mermaidScript = `<script type="module">
-import mermaid from 'https://unpkg.com/mermaid@9/dist/mermaid.esm.min.mjs';
-mermaid.initialize({ startOnLoad: true });
+const DEFAULT_CDN_ADDRESS = 'https://unpkg.com/mermaid@9/dist/mermaid.esm.min.mjs';
+const DEFAULT_CONFIG = '{"startOnLoad":true}';
+
+function genScript(address, config) {
+  return `<script type="module">
+import mermaid from '${address || DEFAULT_CDN_ADDRESS}';
+mermaid.initialize(${JSON.stringify(config) || DEFAULT_CONFIG});
 </script>`;
+}
 
 module.exports = {
   tagConfig: {
@@ -9,8 +14,8 @@ module.exports = {
       isSpecial: true,
     },
   },
-  getScripts: () => [mermaidScript],
-  processNode: (pluginContext, node) => {
+  getScripts: pluginContext => [genScript(pluginContext.address, pluginContext.config)],
+  processNode: (_, node) => {
     if (node.name !== 'mermaid') {
       return;
     }
