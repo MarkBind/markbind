bf16406d0f9a782c9c9176a3fef32b2f34988914
Chan Yu Cheng <e0560244@u.nus.edu>
2023-02-05 12:56:32 +0800

    Remove $

--- packages/vue-components/src/directives/Closeable.js
+++ packages/vue-components/src/directives/Closeable.js
@@ -2,52 +2,52 @@ module.exports = {
   bind(el) {
     function onClose() {
       el.dataset.isShown = 'false';
-      $showLabel.style.display = '';
-      $closeButton.style.display = 'none';
-      $content.style.display = 'none';
+      showLabel.style.display = '';
+      closeButton.style.display = 'none';
+      content.style.display = 'none';
     }
 
     function onShow() {
       el.dataset.isShown = 'true';
-      $showLabel.style.display = 'none';
-      $content.style.display = '';
+      showLabel.style.display = 'none';
+      content.style.display = '';
     }
 
     function onMouseOver() {
       if (el.dataset.isShown === 'false') {
         return;
       }
-      $closeButton.style.display = '';
+      closeButton.style.display = '';
     }
 
     function onMouseOut() {
       if (el.dataset.isShown === 'false') {
         return;
       }
-      $closeButton.style.display = 'none';
+      closeButton.style.display = 'none';
     }
 
     el.dataset.isShown = 'true';
     el.style.position = 'relative';
     const message = el.getAttribute('alt') || 'Expand Content';
-    const $content = document.createElement('div');
+    const content = document.createElement('div');
     el.classList.add('content');
-    Array.from(el.children).forEach(child => $content.append(child));
+    Array.from(el.children).forEach(child => content.append(child));
     el.replaceChildren();
-    el.append($content);
+    el.append(content);
     el.setAttribute('class', `${el.className} closeable-wrapper`);
-    const $closeButton = document.createElement('span');
-    $closeButton.classList.add('closeable-button', 'label', 'label-default', 'hidden-print');
-    $closeButton.style.cssText += 'display: none; position: absolute; top: 0; left: 0; cursor: pointer;background: #d9534f';
-    $closeButton.innerHTML = '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>';
-    el.append($closeButton);
-    const $showLabel = document.createElement('a');
-    $showLabel.classList.add('closeable-show', 'hidden-print');
-    $showLabel.style.cssText += 'display: none; cursor: pointer;text-decoration: underline';
-    $showLabel.innerHTML = message;
-    el.append($showLabel);
-    $closeButton.addEventListener('click', onClose);
-    $showLabel.addEventListener('click', onShow);
+    const closeButton = document.createElement('span');
+    closeButton.classList.add('closeable-button', 'label', 'label-default', 'hidden-print');
+    closeButton.style.cssText += 'display: none; position: absolute; top: 0; left: 0; cursor: pointer;background: #d9534f';
+    closeButton.innerHTML = '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>';
+    el.append(closeButton);
+    const showLabel = document.createElement('a');
+    showLabel.classList.add('closeable-show', 'hidden-print');
+    showLabel.style.cssText += 'display: none; cursor: pointer;text-decoration: underline';
+    showLabel.innerHTML = message;
+    el.append(showLabel);
+    closeButton.addEventListener('click', onClose);
+    showLabel.addEventListener('click', onShow);
     el.addEventListener('mouseover', onMouseOver);
     el.addEventListener('mouseout', onMouseOut);
   }
