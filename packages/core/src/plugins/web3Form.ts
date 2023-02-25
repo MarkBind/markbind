6bd69e333e05cf03dcb01cefe049979f560422dd
Chan Yu Cheng <e0560244@u.nus.edu>
2023-02-25 17:10:51 +0800

    Add submit button if missing

--- packages/core/src/plugins/web3Form.ts
+++ packages/core/src/plugins/web3Form.ts
@@ -15,12 +15,16 @@ const EMAIL_INPUT = '<label for="email">Email</label>\n'
     + '<input type="email" name="email" required placeholder="johndoe@gmail.com">';
 const MESSAGE_INPUT = '<label for="message">Message</label>\n'
     + '<textarea name="message" required></textarea>';
-const SUBMIT_BUTTON = '<button type="submit" class="badge bg-secondary">Submit</button>';
+const SUBMIT_BUTTON = '<button type="submit" class="badge bg-info">Submit</button>';
 
 const DEFAULT_INPUTS = `${NAME_INPUT}\n${EMAIL_INPUT}\n${MESSAGE_INPUT}\n${SUBMIT_BUTTON}`;
 
 const FORM_CONTAINER = '<box type="info" no-icon><h2>Contact Us</h2></box>';
 
+function hasSubmitButton(node: cheerio.Element & DomElement) {
+  const $node = cheerio(node);
+  return $node.children('[type=submit]').length !== 0;
+}
 function createMinimalForm(pluginContext: PluginContext) {
   const $replaceNode = cheerio('<form action="https://api.web3forms.com/submit" method="POST"></form>');
   $replaceNode.append(`<input type="hidden" name="access_key" value=${pluginContext.accessKey}>`);
@@ -30,6 +34,9 @@ function createWeb3Form(pluginContext: PluginContext, node: cheerio.Element & Do
   const $node = cheerio(node);
   const $formNode = createMinimalForm(pluginContext);
   $formNode.append($node.children());
+  if (!hasSubmitButton(node)) {
+    $formNode.append(SUBMIT_BUTTON);
+  }
   const $formWrapper = cheerio(FORM_CONTAINER);
   $formWrapper.append($formNode);
   $node.replaceWith($formWrapper);
