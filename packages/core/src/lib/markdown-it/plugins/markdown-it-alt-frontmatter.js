8884b7a4ccd8d6e66eb8650ef28c6b9ca48d7f94
EltonGohJH <eltongjh@u.nus.edu>
2023-02-25 11:50:06 +0800

    Fix bugs and refactor alternate frontmatter

--- packages/core/src/lib/markdown-it/plugins/markdown-it-alt-frontmatter.js
+++ packages/core/src/lib/markdown-it/plugins/markdown-it-alt-frontmatter.js
@@ -2,55 +2,40 @@
 
 // Process "---" => "<frontmatter>
 
-module.exports = function centertext_plugin(md) {
+module.exports = function alt_frontmatter_plugin(md) {
   function alt_frontmatter(state, startLine, endLine, silent) {
-    var marker, len, nextLine, token,
-      haveEndMarker = false,
-      pos = state.bMarks[startLine] + state.tShift[startLine],
-      max = state.eMarks[startLine];
+    const fmSymbol = "---";
     const keyValueRegex = new RegExp("^\\w+:\\s.*");
 
-    if (pos + 3 > max) { return false; }
-    marker = state.src.slice(pos, pos + 3);
-    if (marker !== '---') { return false; }
+    let pos = state.bMarks[startLine] + state.tShift[startLine];
+    let max = state.eMarks[startLine];
+    let marker = state.src.slice(pos, max);
+    if (marker !== fmSymbol) { return false; }
 
-    // Since we found the marker, we can now search for the end marker
-    // starting from the next line
-    nextLine = startLine;
-    for (;;) {
-      nextLine++;
-      if (nextLine >= endLine) {
-        // unclosed block should be autoclosed by end of document.
-        // also block seems to be autoclosed by end of parent
-        break;
-      }
-
+    let haveEndMarker = false;
+    let nextLine = startLine + 1;
+    for (; nextLine < endLine; nextLine++) {
       pos = state.bMarks[nextLine] + state.tShift[nextLine];
       max = state.eMarks[nextLine];
 
-      if (pos < max && state.sCount[nextLine] < state.blkIndent) {
-        // non-empty line with negative indent should stop the list:
-        // - ```
-        //  test
-        break;
-      }
-
-      if (state.src.slice(pos, max).trim() === '---') {
+      let currLine = state.src.slice(pos, max).trim();
+      if (currLine === fmSymbol) {
         haveEndMarker = true;
         break;
       }
-
-      if (!keyValueRegex.test(state.src.slice(pos, max).trim())) {
+      if (!(keyValueRegex.test(currLine) || currLine.length === 0)) {
+        // terminates if not key value or blank line.
         return false;
       }
     }
+
     if (!haveEndMarker) { return false; }
     // If a fence has heading spaces, they should be removed from its inner block
-    len = state.sCount[startLine];
+    let len = state.sCount[startLine];
     state.line = nextLine + (haveEndMarker ? 1 : 0);
 
-    token = state.push('alt_frontmatter', '', 0);
-    token.info = "---";
+    let token = state.push('alt_frontmatter', '', 0);
+    token.info = fmSymbol;
     token.content = state.getLines(startLine + 1, nextLine, len, true);
     token.markup = marker;
     token.map = [ startLine, state.line ];
@@ -65,6 +50,5 @@ module.exports = function centertext_plugin(md) {
     }
     return slf.renderToken(tokens, idx, options, env, slf);
   };
-
 }
 
