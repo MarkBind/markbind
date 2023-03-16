dbf43e8c203449ae540819787ba764945aa8fd96
Jovyn Tan <jovyn.tls@gmail.com>
2023-03-16 11:03:57 +0800

    Add docs

--- packages/core/src/utils/node.ts
+++ packages/core/src/utils/node.ts
@@ -1,12 +1,22 @@
-import cheerio from 'cheerio';
 import { DomElement } from 'htmlparser2';
 
+/*
+ * A TextElement is a simple node that does not need complex processing.
+ */
 export type TextElement = DomElement;
 
+/*
+ * MbNode (MarkbindNode) is an element that can be operated on by cheerio and our own node processing
+ * methods. It must have a name (used to identify what kind of node it is), attributes (possibly empty),
+ * and children nodes (possibly empty). This type allows us to assert that these attributes exist.
+ */
 export type MbNode = DomElement & cheerio.Element & {
   name: string,
   attribs: { [key: string]: any },
   children: NodeOrText[],
 };
 
+/*
+ * NodeOrText is used before a node can be casted to either TextElement or MbNode.
+ */
 export type NodeOrText = TextElement | MbNode;
