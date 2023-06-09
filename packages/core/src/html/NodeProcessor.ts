db425b4bd917fa3a2f18a3e92b45629b91a95a6d
SPWwj <weiwenjie@u.nus.edu>
2023-06-09 21:33:17 +0800

    Add QR Code Component

--- packages/core/src/html/NodeProcessor.ts
+++ packages/core/src/html/NodeProcessor.ts
@@ -27,6 +27,7 @@ import { highlightCodeBlock, setCodeLineNumbers } from './codeblockProcessor';
 import { setHeadingId, assignPanelId } from './headerProcessor';
 import { FootnoteProcessor } from './FootnoteProcessor';
 import { MbNode, NodeOrText, TextElement } from '../utils/node';
+import { QrCodeProcessor } from './QrCodeProcessor';
 
 const fm = require('fastmatter');
 
@@ -176,90 +177,93 @@ export class NodeProcessor {
       if (_.has(warnConflictingAtributesMap, node.name)) { warnConflictingAtributesMap[node.name](node); }
 
       switch (node.name) {
-      case 'frontmatter':
-        this._processFrontmatter(node, context);
-        break;
-      case 'body':
-        // eslint-disable-next-line no-console
-        console.warn(`<body> tag found in ${node.attribs[ATTRIB_CWF]}. This may cause formatting errors.`);
-        break;
-      case 'include':
-        this.markdownProcessor.docId += 1; // used in markdown-it-footnotes
-        return processInclude(node, context, this.pageSources, this.variableProcessor,
-                              (text: string) => this.markdownProcessor.renderMd(text),
-                              (text: string) => this.markdownProcessor.renderMdInline(text),
-                              this.config);
-      case 'panel':
-        this.mdAttributeRenderer.processPanelAttributes(node);
-        return processPanelSrc(node, context, this.pageSources, this.config);
-      case 'question':
-        this.mdAttributeRenderer.processQuestion(node);
-        break;
-      case 'q-option':
-        this.mdAttributeRenderer.processQOption(node);
-        break;
-      case 'quiz':
-        this.mdAttributeRenderer.processQuiz(node);
-        break;
-      case 'popover':
-        this.mdAttributeRenderer.processPopoverAttributes(node);
-        return processPopoverSrc(node, context, this.pageSources, this.variableProcessor,
-                                 (text: string) => this.markdownProcessor.renderMd(text), this.config);
-      case 'tooltip':
-        this.mdAttributeRenderer.processTooltip(node);
-        break;
-      case 'modal':
-        this.processModal(node);
-        break;
-      case 'tab':
-      case 'tab-group':
-        this.mdAttributeRenderer.processTabAttributes(node);
-        break;
-      case 'box':
-        this.mdAttributeRenderer.processBoxAttributes(node);
-        break;
-      case 'dropdown':
-        this.mdAttributeRenderer.processDropdownAttributes(node);
-        break;
-      case 'thumbnail':
-        this.mdAttributeRenderer.processThumbnailAttributes(node);
-        break;
-      case 'page-nav':
-        this.pageNavProcessor.renderPageNav(node);
-        break;
-      case 'page-nav-print':
-        PageNavProcessor.transformPrintContainer(node);
-        break;
-      case 'site-nav':
-        renderSiteNav(node);
-        break;
-      case 'mb-temp-footnotes':
-        this.footnoteProcessor.processMbTempFootnotes(node);
-        break;
-      case 'script':
-      case 'style':
-        processScriptAndStyleTag(node, this.userScriptsAndStyles);
-        break;
-      case 'scroll-top-button':
-        this.mdAttributeRenderer.processScrollTopButtonAttributes(node);
-        break;
-      case 'code':
-        setCodeLineNumbers(node, this.config.codeLineNumbers);
+        case 'frontmatter':
+          this._processFrontmatter(node, context);
+          break;
+        case 'body':
+          // eslint-disable-next-line no-console
+          console.warn(`<body> tag found in ${node.attribs[ATTRIB_CWF]}. This may cause formatting errors.`);
+          break;
+        case 'include':
+          this.markdownProcessor.docId += 1; // used in markdown-it-footnotes
+          return processInclude(node, context, this.pageSources, this.variableProcessor,
+            (text: string) => this.markdownProcessor.renderMd(text),
+            (text: string) => this.markdownProcessor.renderMdInline(text),
+            this.config);
+        case 'panel':
+          this.mdAttributeRenderer.processPanelAttributes(node);
+          return processPanelSrc(node, context, this.pageSources, this.config);
+        case 'question':
+          this.mdAttributeRenderer.processQuestion(node);
+          break;
+        case 'q-option':
+          this.mdAttributeRenderer.processQOption(node);
+          break;
+        case 'quiz':
+          this.mdAttributeRenderer.processQuiz(node);
+          break;
+        case 'popover':
+          this.mdAttributeRenderer.processPopoverAttributes(node);
+          return processPopoverSrc(node, context, this.pageSources, this.variableProcessor,
+            (text: string) => this.markdownProcessor.renderMd(text), this.config);
+        case 'tooltip':
+          this.mdAttributeRenderer.processTooltip(node);
+          break;
+        case 'modal':
+          this.processModal(node);
+          break;
+        case 'tab':
+        case 'tab-group':
+          this.mdAttributeRenderer.processTabAttributes(node);
+          break;
+        case 'box':
+          this.mdAttributeRenderer.processBoxAttributes(node);
+          break;
+        case 'qr-code':
+          QrCodeProcessor.process(node);
+          break;
+        case 'dropdown':
+          this.mdAttributeRenderer.processDropdownAttributes(node);
+          break;
+        case 'thumbnail':
+          this.mdAttributeRenderer.processThumbnailAttributes(node);
+          break;
+        case 'page-nav':
+          this.pageNavProcessor.renderPageNav(node);
+          break;
+        case 'page-nav-print':
+          PageNavProcessor.transformPrintContainer(node);
+          break;
+        case 'site-nav':
+          renderSiteNav(node);
+          break;
+        case 'mb-temp-footnotes':
+          this.footnoteProcessor.processMbTempFootnotes(node);
+          break;
+        case 'script':
+        case 'style':
+          processScriptAndStyleTag(node, this.userScriptsAndStyles);
+          break;
+        case 'scroll-top-button':
+          this.mdAttributeRenderer.processScrollTopButtonAttributes(node);
+          break;
+        case 'code':
+          setCodeLineNumbers(node, this.config.codeLineNumbers);
         // fall through
-      case 'annotation': // Annotations are added automatically by KaTeX when rendering math formulae.
-      case 'eq': // markdown-it-texmath html tag
-      case 'eqn': // markdown-it-texmath html tag
-      case 'thumb': // image
-        /*
-         * These are not components from MarkBind Vue components.
-         * We have to add 'v-pre' to let Vue know to ignore this tag and not compile it.
-         *
-         * Although there won't be warnings if we use production Vue, it is still good to add this.
-         */
-        if (!_.has(node.attribs, 'v-pre')) { node.attribs['v-pre'] = ''; }
-        break;
-      default:
-        break;
+        case 'annotation': // Annotations are added automatically by KaTeX when rendering math formulae.
+        case 'eq': // markdown-it-texmath html tag
+        case 'eqn': // markdown-it-texmath html tag
+        case 'thumb': // image
+          /*
+           * These are not components from MarkBind Vue components.
+           * We have to add 'v-pre' to let Vue know to ignore this tag and not compile it.
+           *
+           * Although there won't be warnings if we use production Vue, it is still good to add this.
+           */
+          if (!_.has(node.attribs, 'v-pre')) { node.attribs['v-pre'] = ''; }
+          break;
+        default:
+          break;
       }
     } catch (error) {
       logger.error(error);
@@ -274,35 +278,35 @@ export class NodeProcessor {
 
     try {
       switch (node.name) {
-      case 'pre':
-        highlightCodeBlock(node);
-        break;
-      case 'panel':
-        assignPanelId(node);
-        break;
-      case 'head-top': {
-        const collected = NodeProcessor.collectLayoutEl(node);
-        if (collected) {
-          this.headTop.push(collected);
+        case 'pre':
+          highlightCodeBlock(node);
+          break;
+        case 'panel':
+          assignPanelId(node);
+          break;
+        case 'head-top': {
+          const collected = NodeProcessor.collectLayoutEl(node);
+          if (collected) {
+            this.headTop.push(collected);
+          }
+          break;
         }
-        break;
-      }
-      case 'head-bottom': {
-        const collected = NodeProcessor.collectLayoutEl(node);
-        if (collected) {
-          this.headBottom.push(collected);
+        case 'head-bottom': {
+          const collected = NodeProcessor.collectLayoutEl(node);
+          if (collected) {
+            this.headBottom.push(collected);
+          }
+          break;
         }
-        break;
-      }
-      case 'script-bottom': {
-        const collected = NodeProcessor.collectLayoutEl(node);
-        if (collected) {
-          this.scriptBottom.push(collected);
+        case 'script-bottom': {
+          const collected = NodeProcessor.collectLayoutEl(node);
+          if (collected) {
+            this.scriptBottom.push(collected);
+          }
+          break;
         }
-        break;
-      }
-      default:
-        break;
+        default:
+          break;
       }
     } catch (error) {
       logger.error(error);
@@ -329,20 +333,20 @@ export class NodeProcessor {
     }
 
     switch (node.name) {
-    case 'md':
-      node.name = 'span';
-      cheerio(node).html(
-        this.markdownProcessor.renderMdInline(cheerio.html(node.children as unknown as cheerio.Element)),
-      );
-      break;
-    case 'markdown':
-      node.name = 'div';
-      cheerio(node).html(
-        this.markdownProcessor.renderMd(cheerio.html(node.children as unknown as cheerio.Element)),
-      );
-      break;
-    default:
-      break;
+      case 'md':
+        node.name = 'span';
+        cheerio(node).html(
+          this.markdownProcessor.renderMdInline(cheerio.html(node.children as unknown as cheerio.Element)),
+        );
+        break;
+      case 'markdown':
+        node.name = 'div';
+        cheerio(node).html(
+          this.markdownProcessor.renderMd(cheerio.html(node.children as unknown as cheerio.Element)),
+        );
+        break;
+      default:
+        break;
     }
 
     // eslint-disable-next-line no-param-reassign
