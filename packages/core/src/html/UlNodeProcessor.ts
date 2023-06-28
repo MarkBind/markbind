42052d5a4b41941284d49c7cf5ba075281e76eb3
SPWwj <weiwenjie@u.nus.edu>
2023-06-29 02:39:23 +0800

    Complete list level config

--- packages/core/src/html/UlNodeProcessor.ts
+++ packages/core/src/html/UlNodeProcessor.ts
@@ -1,8 +1,11 @@
 import { NodeOrText } from "../utils/node";
+import { EmojiConvertor } from 'emoji-js';
+
 export function processUlNode(node: NodeOrText): NodeOrText {
   if ('name' in node && node.name === 'ul') {
     const iconInUl = node.attribs?.['icon'];
     const iconSize = node.attribs?.['size'];
+    const className = node.attribs?.['class'];
     const hasIconInChildren = node.children?.some((child: NodeOrText) => child.name === 'li' && child.attribs?.['icon']);
 
     if (iconInUl || hasIconInChildren) {
@@ -11,7 +14,7 @@ export function processUlNode(node: NodeOrText): NodeOrText {
       // Update children based on the conditions
       node.children = node.children?.map((child: NodeOrText) => {
         if (child.name === 'li' && (iconInUl || child.attribs?.['icon'])) {
-          updateLiChildren(child, iconInUl, iconSize  );
+          updateLiChildren(child, iconInUl, iconSize, className  );
         }
         return child;
       }) || [];
@@ -36,8 +39,8 @@ function updateNodeStyle(node: NodeOrText) {
   node.attribs['style'] = createStyleString(styleObject);
 }
 
-function updateLiChildren(child: NodeOrText, icon: string, iconSize: string) {
-  const iChild: NodeOrText = createIChild(child, icon, iconSize);
+function updateLiChildren(child: NodeOrText, icon: string, iconSize: string, className?: string) {
+  const iChild: NodeOrText = createIChild(child, icon, iconSize, className);
   const divChild: NodeOrText = createDivChild(child, child.children || []);
   child.children = [iChild, divChild];
   updateNodeRelations(iChild, divChild, child.children || []);
@@ -45,17 +48,93 @@ function updateLiChildren(child: NodeOrText, icon: string, iconSize: string) {
   child.attribs['style'] = 'display: flex;';
 }
 
-function createIChild(parent: NodeOrText, icon: string, iconSize: string ): NodeOrText {
-  return {
-    type: 'tag',
-    name: 'i',
-    attribs: { class: icon, 'aria-hidden': 'true', style: `padding:5px;font-size:${iconSize}` },
-    children: [],
-    next: undefined,
-    prev: undefined,
-    parent: parent
-  };
+function createIChild(parent: NodeOrText, icon: string, iconSize: string, className?: string): NodeOrText {
+  const emoji = new EmojiConvertor();
+  emoji.replace_mode = 'unified';
+  emoji.allow_native = true;
+
+  const emojiString = `:${icon}:`;
+  const unicodeEmoji = emoji.replace_colons(emojiString);
+
+  const isEmoji = unicodeEmoji !== emojiString;
+  const localFileRegex = /^(\.\/)?[a-zA-Z0-9\-._~:/?#[\]@!$&'()*+,;=%]+\.(jpg|png|gif|bmp|svg|jpeg)$/;
+  const urlRegex = /^(http(s)?|ftp):\/\/[^\s/$.?#].[^\s]*$/;
+  const isImage = !isEmoji && (localFileRegex.test(icon) || urlRegex.test(icon));
+  const size = getSize(iconSize);
+
+  let child: NodeOrText;
+
+  if (isEmoji) {
+    child = {
+      type: 'tag',
+      name: 'i',
+      attribs: {
+        ...className && { class: className },
+        'aria-hidden': 'true',
+        style: size.fontSize ? `font-size:${size.fontSize}; margin-right: 5px;` : 'margin-right: 5px;'
+      },
+      children: [{
+        type: 'text',
+        data: unicodeEmoji,
+      }],
+      next: undefined,
+      prev: undefined,
+      parent: parent
+    };
+  } else if (isImage) {
+    const altText = icon.split('/').pop()?.split('.')[0] || '';
+
+    child = {
+      type: 'tag',
+      name: 'img',
+      attribs: {
+        ...className && { class: className },
+        src: icon,
+        alt: altText,
+        style: `width: ${size.imageSize}; height: ${size.imageSize}; margin-right: 5px;`
+      },
+      children: [],
+      next: undefined,
+      prev: undefined,
+      parent: parent,
+    };
+  } else {
+    child = {
+      type: 'tag',
+      name: 'i',
+      attribs: {
+        class: className ? `${icon} ${className}` : icon,
+        'aria-hidden': 'true',
+        style: `padding:5px;margin-right:5px;${size.fontSize ? `font-size:${size.fontSize};` : ''}`
+      },
+      children: [],
+      next: undefined,
+      prev: undefined,
+      parent: parent,
+    };
+  }
+  return child;
 }
+
+function getSize(iconSize :string) {
+  iconSize = (iconSize || 'xs').toLowerCase();
+
+  switch (iconSize) {
+    case 's':
+      return { fontSize: '12px', imageSize: '30px' };
+    case 'm':
+      return { fontSize: '18px', imageSize: '35px' };
+    case 'l':
+      return { fontSize: '24px', imageSize: '50px' };
+    case 'xl':
+      return { fontSize: '30px', imageSize: '65px' };
+    default: // 'xs'
+      return { fontSize: undefined, imageSize: '25px' };
+  }
+}
+
+
+
 
 function createDivChild(parent: NodeOrText, children: NodeOrText[]): NodeOrText {
   return {
