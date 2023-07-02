21dd1a471e04360c1d8b28514b8ed75074ff3f5b
SPWwj <weiwenjie@u.nus.edu>
2023-07-02 21:16:00 +0800

    Fix lint errors

--- packages/core/src/html/UlNodeProcessor.ts
+++ packages/core/src/html/UlNodeProcessor.ts
@@ -1,6 +1,7 @@
-import { IconSize } from "@primer/octicons";
-import { NodeOrText } from "../utils/node";
 import { EmojiConvertor } from 'emoji-js';
+import isNumber from 'lodash/isNumber';
+import isString from 'lodash/isString';
+import { NodeOrText } from '../utils/node';
 
 interface IconAttributes {
   icon?: string;
@@ -8,88 +9,45 @@ interface IconAttributes {
   className?: string;
 }
 
-export function processUlNode(node: NodeOrText): NodeOrText {
-  const iconUl = node.attribs?.['icon'];
-  const hasIconInChildren = node.children?.find((child: NodeOrText) => child.name === 'li' && child.attribs?.['icon']);
-  if (iconUl == undefined && hasIconInChildren == undefined) return node;
+type Size = {
+  fontSize: string;
+  imageSize: string;
+  squareSize: string;
+};
 
-  if ('name' in node && node.name === 'ul') {
-    const iconAttributes: IconAttributes = {
-      icon: iconUl,
-      iconSize: node.attribs?.['size'],
-      className: node.attribs?.['class']
-    };
+function updateNodeRelations(iChild: NodeOrText, divChild: NodeOrText, children: NodeOrText[]): void {
+  iChild.next = divChild;
+  divChild.prev = iChild;
 
-    // Delete the attributes after getting their values
-    if (iconAttributes.icon) delete node.attribs?.['icon'];
-    if (iconAttributes.iconSize) delete node.attribs?.['size'];
-    if (iconAttributes.className) delete node.attribs?.['class'];
+  children.forEach((child) => {
+    child.parent = divChild;
+  });
+}
 
-
-
-    let defaultLiIcon: IconAttributes = {};
-    if (hasIconInChildren) {
-      defaultLiIcon = {
-        icon: hasIconInChildren.attribs?.['icon'],
-        iconSize: hasIconInChildren.attribs?.['size'],
-        className: hasIconInChildren.attribs?.['class']
-      };
-    }
-
-    if (iconAttributes.icon || hasIconInChildren) {
-      updateNodeStyle(node);
-
-      // Update children based on the conditions
-      node.children = node.children?.map((child: NodeOrText) => {
-        if (child.name === 'li') {
-          updateLiChildren(child, iconAttributes, defaultLiIcon);
-        }
-        return child;
-      }) || [];
-    }
+function getStyleObject(node: NodeOrText): { [key: string]: string } {
+  let styleObject: { [key: string]: string } = {};
+  if (node.attribs?.style) {
+    const { style } = node.attribs;
+    styleObject = style.split(';').reduce((acc: Record<string, string>, rule: string) => {
+      const [property, value] = rule.split(':').map((str: string) => str.trim());
+      if (property) acc[property] = value;
+      return acc;
+    }, {});
   }
-
-  // If it's not a 'ul' node, return the node as is
-  return node;
+  return styleObject;
 }
 
-function updateNodeStyle(node: NodeOrText) {
-  // Initialize styles object
-  let styleObject = getStyleObject(node);
-
-  // Add or overwrite list-style-type and display
-  styleObject['list-style-type'] = 'none';
-  styleObject['padding-left'] = '0';
-  // Ensure node.attribs is defined before use
-  if (!node.attribs) node.attribs = {};
-
-  // Recreate style string and assign to style attribute
-  node.attribs['style'] = createStyleString(styleObject);
+function createStyleString(styleObject: { [key: string]: string }): string {
+  return Object.entries(styleObject).map(
+    ([property, value]: [string, string]) => `${property}: ${value}`).join('; ');
 }
 
-function updateLiChildren(child: NodeOrText, parentIconAttributes: IconAttributes, defaultLiIconAttributes: IconAttributes) {
-  // Get the child's attributes, these will override the parent's attributes
-  const icon = child.attribs?.['icon'] || parentIconAttributes.icon || defaultLiIconAttributes.icon;
-  const iconSize = child.attribs?.['size'] || parentIconAttributes.iconSize || defaultLiIconAttributes.iconSize;
-  const className = child.attribs?.['class'] || parentIconAttributes.className || defaultLiIconAttributes.className;
-
-  // Delete the child's attributes after getting their values
-  if (child.attribs?.['icon']) delete child.attribs['icon'];
-  if (child.attribs?.['size']) delete child.attribs['size'];
-  if (child.attribs?.['class']) delete child.attribs['class'];
-
-  const size = getSize(iconSize);
-
-  const iChild: NodeOrText = createIChild(child, icon, size, className);
-  const divChild: NodeOrText = createDivChild(child, child.children || []);
-  child.children = [iChild, divChild];
-  updateNodeRelations(iChild, divChild, child.children || []);
-  if (!child.attribs) child.attribs = {};
-  child.attribs['style'] = 'display: flex;';
-}
-
-
-function createIChild(parent: NodeOrText, icon: string, size: Size, className?: string): NodeOrText {
+function createIChild(
+  parent: NodeOrText,
+  icon: string,
+  size: Size,
+  className?: string,
+): NodeOrText {
   const emoji = new EmojiConvertor();
   emoji.replace_mode = 'unified';
   emoji.allow_native = true;
@@ -113,15 +71,18 @@ function createIChild(parent: NodeOrText, icon: string, size: Size, className?: 
       attribs: {
         ...className && { class: className },
         'aria-hidden': 'true',
-        style: defaultSize + (size.fontSize ? `font-size:${size.fontSize}; margin-right: 5px;` : 'margin-right: 5px;'),
+        style: `${defaultSize}${size.fontSize ? `font-size:${size.fontSize};
+        margin-right: 5px;` : 'margin-right: 5px;'}`,
       },
-      children: [{
-        type: 'text',
-        data: unicodeEmoji,
-      }],
+      children: [
+        {
+          type: 'text',
+          data: unicodeEmoji,
+        },
+      ],
       next: undefined,
       prev: undefined,
-      parent: parent
+      parent,
     };
   } else if (isImage) {
     const altText = icon.split('/').pop()?.split('.')[0] || '';
@@ -129,12 +90,12 @@ function createIChild(parent: NodeOrText, icon: string, size: Size, className?: 
       type: 'tag',
       name: 'div',
       attribs: {
-        style: "padding: 0.2rem;",
+        style: 'padding: 0.2rem;',
       },
       children: [],
       next: undefined,
       prev: undefined,
-      parent: parent,
+      parent,
     };
 
     const imgNode: NodeOrText = {
@@ -160,34 +121,30 @@ function createIChild(parent: NodeOrText, icon: string, size: Size, className?: 
       attribs: {
         class: className ? `${icon} ${className}` : icon,
         'aria-hidden': 'true',
-        style: defaultSize + `margin-right:5px;${size.fontSize ? `font-size:${size.fontSize};` : ''}`
+        style: `${defaultSize}margin-right:5px;${size.fontSize ? `font-size:${size.fontSize};` : ''}`,
       },
       children: [],
       next: undefined,
       prev: undefined,
-      parent: parent,
+      parent,
     };
   }
   return child;
 }
-type Size = {
-  fontSize: string;
-  imageSize: string;
-  squareSize: string;
-};
 
-function getSize(iconSize = "25px") {
-  if (typeof iconSize === 'number') {
+function getSize(iconSizeInput = '25px') {
+  let iconSize = iconSizeInput;
+  if (isNumber(iconSize)) {
     // If iconSize is a number, convert it to a string and add 'px'
     iconSize = `${iconSize}px`;
-  } else if (typeof iconSize !== 'string' || !/^\d+px$/.test(iconSize)) {
+  } else if (!isString(iconSize) || !/^\d+px$/.test(iconSize)) {
     // If iconSize is not a string in the format 'nnnpx', default to '25px'
     iconSize = '25px';
   }
   // Remove 'px' from the end and convert to a number
-  let n = Number(iconSize.slice(0, -2));
+  const n = Number(iconSize.slice(0, -2));
 
-  // Calculate fontSize based on the size of the square and image. 
+  // Calculate fontSize based on the size of the square and image.
   // The multiplier (0.64) is approximated from the relationship in the provided sizes.
   let fontSize = 16 + (n - 25) * 0.64;
 
@@ -201,45 +158,85 @@ function getSize(iconSize = "25px") {
   };
 }
 
-
-
-
-
 function createDivChild(parent: NodeOrText, children: NodeOrText[]): NodeOrText {
   return {
     type: 'tag',
     name: 'div',
     attribs: {},
-    children: children,
+    children,
     next: undefined,
     prev: undefined,
-    parent: parent
+    parent,
   };
 }
 
-function updateNodeRelations(iChild: NodeOrText, divChild: NodeOrText, children: NodeOrText[]): void {
-  iChild.next = divChild;
-  divChild.prev = iChild;
+function updateNodeStyle(node: NodeOrText) {
+  const styleObject = getStyleObject(node);
+  styleObject['list-style-type'] = 'none';
+  styleObject['padding-left'] = '0';
+  if (!node.attribs) node.attribs = {};
 
-  children.forEach(child => {
-    child.parent = divChild;
-  });
+  node.attribs.style = createStyleString(styleObject);
 }
 
+function updateLiChildren(child: NodeOrText, parentIconAttributes:
+IconAttributes, defaultLiIconAttributes: IconAttributes) {
+  const icon = child.attribs?.icon || parentIconAttributes.icon || defaultLiIconAttributes.icon;
+  const iconSize = child.attribs?.size || parentIconAttributes.iconSize || defaultLiIconAttributes.iconSize;
+  const className = child.attribs?.class
+                        || parentIconAttributes.className
+                        || defaultLiIconAttributes.className;
 
-function getStyleObject(node: NodeOrText): { [key: string]: string } {
-  let styleObject: { [key: string]: string } = {};
-  if (node.attribs?.['style']) {
-    const style = node.attribs['style'];
-    styleObject = style.split(';').reduce((acc: Record<string, string>, rule: string) => {
-      const [property, value] = rule.split(':').map((str: string) => str.trim());
-      if (property) acc[property] = value;
-      return acc;
-    }, {});
+  if (child.attribs?.icon) delete child.attribs.icon;
+  if (child.attribs?.size) delete child.attribs.size;
+  if (child.attribs?.class) delete child.attribs.class;
+
+  const size = getSize(iconSize);
+
+  const iChild: NodeOrText = createIChild(child, icon, size, className);
+  const divChild: NodeOrText = createDivChild(child, child.children || []);
+  child.children = [iChild, divChild];
+  updateNodeRelations(iChild, divChild, child.children || []);
+  if (!child.attribs) child.attribs = {};
+  child.attribs.style = 'display: flex;';
+}
+
+export function processUlNode(node: NodeOrText): NodeOrText {
+  const iconUl = node.attribs?.icon;
+  const hasIconInChildren = node.children?.find(
+    (child: NodeOrText) => child.name === 'li' && child.attribs?.icon,
+  );
+  if (iconUl === undefined && hasIconInChildren === undefined) return node;
+
+  if ('name' in node && node.name === 'ul') {
+    const iconAttributes: IconAttributes = {
+      icon: iconUl,
+      iconSize: node.attribs?.size,
+      className: node.attribs?.class,
+    };
+
+    if (iconAttributes.icon) delete node.attribs?.icon;
+    if (iconAttributes.iconSize) delete node.attribs?.size;
+    if (iconAttributes.className) delete node.attribs?.class;
+
+    let defaultLiIcon: IconAttributes = {};
+    if (hasIconInChildren) {
+      defaultLiIcon = {
+        icon: hasIconInChildren.attribs?.icon,
+        iconSize: hasIconInChildren.attribs?.size,
+        className: hasIconInChildren.attribs?.class,
+      };
+    }
+
+    if (iconAttributes.icon || hasIconInChildren) {
+      updateNodeStyle(node);
+      node.children = node.children?.map((child: NodeOrText) => {
+        if (child.name === 'li') {
+          updateLiChildren(child, iconAttributes, defaultLiIcon);
+        }
+        return child;
+      }) || [];
+    }
   }
-  return styleObject;
-}
-
-function createStyleString(styleObject: { [key: string]: string }): string {
-  return Object.entries(styleObject).map(([property, value]: [string, string]) => `${property}: ${value}`).join('; ');
+  return node;
 }
