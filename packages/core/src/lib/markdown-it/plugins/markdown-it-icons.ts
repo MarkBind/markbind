0e0aa0d2cb8aca620dffe8085c928d2e8361e84a
SPWwj <weiwenjie@u.nus.edu>
2023-07-19 14:50:21 +0800

    Update markdown-it-icons to include comments

--- packages/core/src/lib/markdown-it/plugins/markdown-it-icons.ts
+++ packages/core/src/lib/markdown-it/plugins/markdown-it-icons.ts
@@ -1,26 +1,34 @@
+// import the necessary packages
 const octicons = require('@primer/octicons');
 const cheerio = require('cheerio');
 const markdownItRegExp = require('markdown-it-regexp');
 
+// regular expression to match the icon patterns
 const ICON_REGEXP = /:(fa[brs]|glyphicon|octicon|octiconlight|mi[forst])-([a-z-]+)~?([a-z-]+)?:/;
 
+// function to get the octicon icons
 function getOcticonIcon(iconName: string) {
   return octicons[iconName] ?? null;
 }
 
+// handler function for glyphicon icons
 const handleGlyphicon = (iconFontName: string) =>
   `<span aria-hidden="true" class="glyphicon glyphicon-${iconFontName}"></span>`;
 
+// handler function for octicon icons
 const handleOcticon = (iconFontName: string, iconClass?: string) => {
   const octiconIcon = getOcticonIcon(iconFontName);
+  // ensure octicons are valid
   if (octiconIcon === null) {
     return '<span aria-hidden="true"></span>';
   }
   return iconClass ? octiconIcon.toSVG({ class: iconClass }) : octiconIcon.toSVG();
 };
 
+// handler function for light octicon icons
 const handleOcticonLight = (iconFontName: string, iconClass?: string) => {
   const octiconIcon = getOcticonIcon(iconFontName);
+  // ensure octicons are valid
   if (octiconIcon === null) {
     return '<span aria-hidden="true"></span>';
   }
@@ -30,6 +38,7 @@ const handleOcticonLight = (iconFontName: string, iconClass?: string) => {
   return $.html();
 };
 
+// handler function for material icons
 const handleMaterialIcon = (iconFontType: string, iconFontName: string) => {
   let materialIconsClass = 'material-icons';
   switch (iconFontType) {
@@ -46,14 +55,19 @@ const handleMaterialIcon = (iconFontType: string, iconFontName: string) => {
     materialIconsClass += '-two-tone';
     break;
   default:
+  // .material-icons generates 'Filled' style icons; hence, no suffix is needed for 'mif'.
   }
+  // Use .align-middle by default to vertically-align the icon with its surrounding text (if any).
+  // Also, replace dashes (-) with underscores (_) to format the icon name properly.
   return `<span aria-hidden="true" class="${materialIconsClass} align-middle">`
     + `${iconFontName.split('-').join('_')}</span>`;
 };
 
+// handler function for font awesome icons
 const handleFontAwesome = (iconFontType: string, iconFontName: string) =>
   `<span aria-hidden="true" class="${iconFontType} fa-${iconFontName}"></span>`;
 
+// function to get the respective icon html based on the icon font type
 const getIconHtml = (match: string[]) => {
   const iconFontType = match[1];
   const iconFontName = match[2];
@@ -71,6 +85,7 @@ const getIconHtml = (match: string[]) => {
   return handleFontAwesome(iconFontType, iconFontName);
 };
 
+// function to process the icon string and get the icon html
 const processIconString = (iconStr: string) => {
   let icon = iconStr;
 
@@ -82,6 +97,7 @@ const processIconString = (iconStr: string) => {
   return match ? getIconHtml(match) : null;
 };
 
+// create a markdown-it plugin to process the icon strings in the markdown
 const markdownItPlugin = markdownItRegExp(
   ICON_REGEXP,
   (match: string[]) => getIconHtml(match),
@@ -90,5 +106,5 @@ const markdownItPlugin = markdownItRegExp(
 // Attach `getIconHtml` to the exported function
 markdownItPlugin.processIconString = processIconString;
 
-// The export remains unchanged
+// export the markdown-it plugin
 module.exports = markdownItPlugin;
