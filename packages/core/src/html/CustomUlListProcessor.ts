137b52b73716b52b1e305b337e7ae7b0ac57c94d
SPWwj <weiwenjie@u.nus.edu>
2023-07-31 22:52:53 +0800

    Update comment

--- packages/core/src/html/CustomUlListProcessor.ts
+++ packages/core/src/html/CustomUlListProcessor.ts
@@ -84,7 +84,8 @@ function updateNodeStyle(node: NodeOrText) {
   });
 }
 
-// If an item has a specified icon, that icon will be used for it and for subsequent items at that level.
+// If an item has a specified icon, that icon will be used for it and for subsequent 
+// items at that level to prevent duplication of icons attribute declarations.
 const getIconAttributes = (node: MbNode, iconAttrsSoFar?: IconAttributes):
 IconAttributes | null => {
   if (iconAttrsSoFar?.icon === undefined && node.attribs.icon === undefined) {
@@ -130,7 +131,8 @@ function updateLi(node: MbNode, iconAttributes: IconAttributes) {
 }
 
 // This function ensure the first item of at that level must also be customized.
-// If not, the list will be invalidated and default bullets will be used.
+// If not, the list will be invalidated and default bullets will be used. 
+// This is to prevent unintentional mixing of standard and customized lists.
 function handleLiNode(node: MbNode, iconAttrValue: IconAttributeDetail) {
   if (iconAttrValue.isFirst) {
     iconAttrValue.iconAttrs = getIconAttributes(node);
