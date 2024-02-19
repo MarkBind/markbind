f342c87351e20e5184c1b475dc6a105a3b2beaa3
jingting1412 <jingting.sg@gmail.com>
2024-02-19 12:37:58 +0800

    Add functional test

---
+++ packages/cli/test/functional/test_site_templates/test_portfolio/expected/404.page-vue-render.js
@@ -0,0 +1,10 @@
+
+    var pageVueRenderFn = function anonymous(
+) {
+with(this){return _m(0)}
+};
+    var pageVueStaticRenderFns = [function anonymous(
+) {
+with(this){return _c('div',{attrs:{"id":"app"}},[_c('div',[_c('p'),_c('div',{staticClass:"text-center"},[_c('p',{staticStyle:{"font-size":"10rem"}},[_v("404")])]),_v(" "),_c('div',{staticClass:"text-center"},[_c('p',{staticStyle:{"font-size":"1.5rem"}},[_v("File not found"),_c('br'),_v("Click "),_c('a',{attrs:{"href":"/"}},[_v("here")]),_v(" to go back to the home page.")])]),_c('p')])])}
+}];
+  
