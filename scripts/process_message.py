4198d8248f5529aa8c2a9ecee56a0c7af4755397
KevinEyo1 <eyokaiwenkevin@gmail.com>
2024-02-21 23:51:30 +0800

    Add script

---
+++ scripts/process_message.py
@@ -0,0 +1,17 @@
+import json
+import re
+import sys
+
+markdown_content = sys.argv[1]
+
+# Preprocessing the markdown content
+markdown_content = markdown_content.replace('`', '\\`')
+markdown_content = re.sub(r'<!--.*?-->', '', markdown_content, flags=re.DOTALL)  # Remove HTML comments
+markdown_content = re.sub(r'^\s+', '', markdown_content, flags=re.MULTILINE)  # Trim leading whitespace
+markdown_content = re.sub(r'\s+$', '', markdown_content, flags=re.MULTILINE)  # Trim trailing whitespace
+markdown_content = markdown_content.replace('\n', '\\n').replace('\r', '').replace('\t', '\\t')
+markdown_content = markdown_content.replace('(', '\\(').replace(')', '\\)')
+
+print(markdown_content)
+
+
