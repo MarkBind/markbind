26dac2bdeefc526301875bf63a079233e9808aa3
KevinEyo1 <eyokaiwenkevin@gmail.com>
2024-02-22 17:50:29 +0800

    Fix parsing

--- scripts/process_message.py
+++ scripts/process_message.py
@@ -5,16 +5,15 @@ import sys
 markdown_content = sys.argv[1]
 
 # Preprocessing the markdown content
+# add eof
 markdown_content = markdown_content.replace('`', '\\`')
-markdown_content = markdown_content.replace('*', '\\*')
-markdown_content = markdown_content.replace('#', '\\#')
 markdown_content = markdown_content.replace('\n', '\\n').replace('\r', '').replace('\t', '\\t')
 markdown_content = markdown_content.replace('(', '\\(').replace(')', '\\)')
 markdown_content = re.sub(r'<!--.*?-->', '', markdown_content, flags=re.DOTALL)  # Remove HTML comments
 markdown_content = re.sub(r'^\s+', '', markdown_content, flags=re.MULTILINE)  # Trim leading whitespace
 markdown_content = re.sub(r'\s+$', '', markdown_content, flags=re.MULTILINE)  # Trim trailing whitespace
 
-print(json.dumps({"content": markdown_content}))
+print(markdown_content)
 
 
 
