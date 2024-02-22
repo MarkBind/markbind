7b2feb4d73d65d4dbbbed059d43662ef07030048
KevinEyo1 <eyokaiwenkevin@gmail.com>
2024-02-22 15:54:02 +0800

    Fix link errors

--- scripts/process_message.py
+++ scripts/process_message.py
@@ -2,15 +2,18 @@ import json
 import re
 import sys
 
-markdown_content = sys.argv[1]
+markdown_file_path = sys.argv[1]
+
+with open(markdown_file_path, 'r', encoding='utf-8') as file:
+    markdown_content = file.read()
 
 # Preprocessing the markdown content
 markdown_content = markdown_content.replace('`', '\\`')
+markdown_content = markdown_content.replace('\n', '\\n').replace('\r', '').replace('\t', '\\t')
+markdown_content = markdown_content.replace('(', '\\(').replace(')', '\\)')
 markdown_content = re.sub(r'<!--.*?-->', '', markdown_content, flags=re.DOTALL)  # Remove HTML comments
 markdown_content = re.sub(r'^\s+', '', markdown_content, flags=re.MULTILINE)  # Trim leading whitespace
 markdown_content = re.sub(r'\s+$', '', markdown_content, flags=re.MULTILINE)  # Trim trailing whitespace
-markdown_content = markdown_content.replace('\n', '\\n').replace('\r', '').replace('\t', '\\t')
-markdown_content = markdown_content.replace('(', '\\(').replace(')', '\\)')
 
 print(markdown_content)
 
