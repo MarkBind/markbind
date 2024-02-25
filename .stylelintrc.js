1967a6a4dcbc20244f8ca285b5b2c88cc604e4d8
Jiu Fong Lam <>
2024-02-25 19:22:24 +0800

    Add plugin @stylistic/stylelint-plugin and postcss@^8.4.35

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   "extends": ["stylelint-config-standard", "stylelint-config-recommended-vue"],
+  "plugins": ["@stylistic/stylelint-plugin"],
   "rules": {
-    // "indentation": [ 4, { baseIndentLevel: 1 } ],
+    "@stylistic/indentation": [4, { baseIndentLevel: 1 } ],
     // MarkBind generates some blank CSS files when initialising a site,
     // which violates the no-empty-source rule
     "no-empty-source": null
