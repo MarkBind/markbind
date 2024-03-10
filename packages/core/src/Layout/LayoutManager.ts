eb2d13dcc8fb2dd9367f1173ec7fbb7236ce2b92
Wang Yiwen <e0968880@u.nus.edu>
2024-03-10 23:28:04 +0800

    Adapt core/src/Layout to TypeScript

--- packages/core/src/Layout/LayoutManager.ts
+++ packages/core/src/Layout/LayoutManager.ts
@@ -1,32 +1,32 @@
-const fs = require('fs-extra');
-const path = require('path');
-
-const { Layout } = require('./Layout');
-
-const logger = require('../utils/logger');
+import fs from 'fs-extra';
+import path from 'path';
+import { Layout, LayoutConfig, PageNjkAssets } from './Layout';
+import * as logger from '../utils/logger';
 
 const FRONTMATTER_NONE_ATTR = 'none';
 
-class LayoutManager {
-  constructor(config) {
+export class LayoutManager {
+  private config: LayoutConfig;
+  private layoutsRootPath: string;
+  private layouts: { [name: string]: Layout };
+
+  constructor(config: LayoutConfig) {
     this.config = config;
-
     this.layoutsRootPath = path.join(config.rootPath, '_markbind', 'layouts');
-
     this.layouts = {};
   }
 
   /**
    * Flag all layouts for (re)generation when requested
    */
-  removeLayouts() {
+  removeLayouts(): void {
     this.layouts = {};
   }
 
   /**
    * Update layouts which have the provided filePaths as dependencies
    */
-  updateLayouts(filePaths) {
+  updateLayouts(filePaths: string[]): Promise<void[]> {
     const layoutsToRegenerate = Object.entries(this.layouts)
       .filter(([, layout]) => layout.shouldRegenerate(filePaths));
 
@@ -36,7 +36,7 @@ class LayoutManager {
     }));
   }
 
-  generateLayoutIfNeeded(name) {
+  generateLayoutIfNeeded(name: string): Promise<void> | undefined {
     if (this.layouts[name]) {
       return this.layouts[name].generatePromise;
     }
@@ -52,15 +52,18 @@ class LayoutManager {
     return this.layouts[name].generatePromise;
   }
 
-  layoutHasPageNav(name) {
+  layoutHasPageNav(name: string): boolean {
     if (name === FRONTMATTER_NONE_ATTR) {
       return false;
     }
 
-    return this.layouts[name] && this.layouts[name].layoutPageNavUuid;
+    return !!this.layouts[name] && !!this.layouts[name].layoutPageNavUuid;
   }
 
-  combineLayoutWithPage(name, pageContent, pageNav, pageIncludedFiles) {
+  combineLayoutWithPage(name: string,
+                        pageContent: string,
+                        pageNav: string,
+                        pageIncludedFiles: Set<string>): string {
     if (name === FRONTMATTER_NONE_ATTR) {
       return pageContent;
     }
@@ -72,7 +75,7 @@ class LayoutManager {
     return this.layouts[name].insertPage(pageContent, pageNav, pageIncludedFiles);
   }
 
-  getLayoutPageNjkAssets(name) {
+  getLayoutPageNjkAssets(name: string): PageNjkAssets | {} {
     if (name === FRONTMATTER_NONE_ATTR || !this.layouts[name]) {
       return {};
     }
@@ -80,7 +83,3 @@ class LayoutManager {
     return this.layouts[name].getPageNjkAssets();
   }
 }
-
-module.exports = {
-  LayoutManager,
-};
