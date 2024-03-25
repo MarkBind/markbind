a22da8e3c0e7e20735365b54623ade44c13e085b
Tim-Siu <shuyao@u.nus.edu>
2024-03-25 20:45:58 +0800

    Improve code quality

--- packages/core/src/plugins/mermaid.ts
+++ packages/core/src/plugins/mermaid.ts
@@ -1,18 +1,16 @@
-import { PluginContext } from './Plugin';
-import { NodeOrText } from '../utils/node';
+import cheerio from 'cheerio';
+import { PluginContext, FrontMatter } from './Plugin';
 
 const DEFAULT_CDN_ADDRESS = 'https://unpkg.com/mermaid@9/dist/mermaid.esm.min.mjs';
-const DEFAULT_CONFIG = '{"startOnLoad":true}';
 
-function genScript(address: string, config: string) {
+function genScript(address: string) {
   return `<script type="module">
-Vue.directive('mermaid', {
-  inserted: function(el) {
-    mermaid.init(undefined, el);
-  }});
-import mermaid from '${address || DEFAULT_CDN_ADDRESS}';
-mermaid.initialize(${JSON.stringify(config) || DEFAULT_CONFIG});
-</script>`;
+    import mermaid from '${address || DEFAULT_CDN_ADDRESS}';
+    Vue.directive('mermaid', {
+      inserted: function(el) {
+        mermaid.init(undefined, el);
+      }});
+  </script>`;
 }
 
 export = {
@@ -21,17 +19,15 @@ export = {
       isSpecial: true,
     },
   },
-  getScripts: (pluginContext: PluginContext) => [genScript(pluginContext.address, pluginContext.config)],
-  processNode: (_: PluginContext, node: NodeOrText) => {
-    if (node.name !== 'mermaid') {
-      return;
-    }
-    node.name = 'div';
-    if (!node.attribs) {
-      node.attribs = {};
-    } else {
-      node.attribs.class = 'mermaid';
-      node.attribs['v-mermaid'] = '';
-    }
+  getScripts: (pluginContext: PluginContext) => [genScript(pluginContext.address)],
+  postRender: (pluginContext: PluginContext, frontmatter: FrontMatter, content: string) => {
+    const $ = cheerio.load(content);
+
+    $('mermaid').each((index: number, node: cheerio.Element) => {
+      const $node = $(node);
+      $node.replaceWith(`<div class="mermaid" v-mermaid>${$node.html()}</div>`);
+    });
+
+    return $.html();
   },
 };
