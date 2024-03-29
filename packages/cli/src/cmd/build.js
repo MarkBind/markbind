d8e49369806fb7a67370a7511431d767ef53920f
jingting1412 <jingting.sg@gmail.com>
2024-03-29 22:35:18 +0800

    Add pagefind as dependency

--- packages/cli/src/cmd/build.js
+++ packages/cli/src/cmd/build.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const { Site } = require('@markbind/core').Site;
 
+const pagefind = require('pagefind');
 const cliUtil = require('../util/cliUtil');
 const logger = require('../util/logger');
 
@@ -26,6 +27,15 @@ function build(userSpecifiedRoot, output, options) {
     .generate(baseUrl)
     .then(() => {
       logger.info('Build success!');
+      // Start Pagefind indexing after successful site generation
+      logger.info('Starting Pagefind indexing...');
+      const { index } = pagefind.createIndex({
+        keepIndexUrl: true,
+        verbose: true,
+      })
+        .then(() => index.addDirectory({ path: outputFolder }))
+        .then(() => index.writeFiles({ outputPath: path.join(outputFolder, 'pagefind') }))
+        .then(() => pagefind.close());
     })
     .catch((error) => {
       logger.error(error.message);
--- packages/core/src/plugins/pageFind.ts
+++ packages/core/src/plugins/pageFind.ts
@@ -3,11 +3,10 @@ import { PluginContext } from './Plugin';
 import { MbNode } from '../utils/node';
 
 const DEFAULT_CDN_ADDRESS = 'https://cdn.jsdelivr.net/npm/pagefind@1.0.4/lib/index.min.js';
+const DEFAULT_UI = 'https://cdn.jsdelivr.net/npm/@pagefind/default-ui@1.0.4/+esm';
 
 function initPagefind() {
-  return `<script src='${DEFAULT_CDN_ADDRESS}'></script>;
-  <script>
-    const { default: pagefind } = await import(DEFAULT_CDN_ADDRESS);
+  return `<script>
     const { index } = await pagefind.createIndex({
       keepIndexUrl: true,
       verbose: true,
@@ -20,7 +19,7 @@ function initPagefind() {
 
 function addPagefindUI() {
   return `
-<link rel="stylesheet" href="/pagefind/pagefind-ui.css">
+<link rel="stylesheet" href="${DEFAULT_UI}">
 <script src="/pagefind/pagefind-ui.js"></script>
 <div id="search"></div>
 <script>
@@ -39,5 +38,5 @@ export = {
     const $ = cheerio.load(node);
     $('header').append(addPagefindUI());
   },
-  getScripts: () => [initPagefind()],
+  getScripts: () => [`<script src='${DEFAULT_CDN_ADDRESS}'></script>`, initPagefind()],
 };
