7feb0ed41b6502a208f342e5337d6d14ca8342a9
KevinEyo1 <eyokaiwenkevin@gmail.com>
2024-03-30 21:19:46 +0800

    Merge changes

--- packages/cli/test/functional/test_site_templates/test_project/expected/developerGuide/SettingUp.page-vue-render.js
+++ packages/cli/test/functional/test_site_templates/test_project/expected/developerGuide/SettingUp.page-vue-render.js
@@ -38,6 +38,6 @@ with(this){return _c('li',[_c('p',[_c('strong',[_v("Learn the design")])]),_v(" 
 with(this){return _c('li',[_c('p',[_c('strong',[_v("Do the tutorial")])]),_v(" "),_c('p',[_v("The "),_c('a',{attrs:{"href":"/developerGuide/TracingCode.html"}},[_v("Tracing Code tutorial")]),_v(" can help you get acquainted with the codebase.")])])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.3.0")]),_v("]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.4.0")]),_v("]")])])])}
 }];
   
