ce9af6e49f92bc06ac5643520d1ac0b5fd97c297
AgentHagu <adrianleonardoliang@gmail.com>
2025-03-01 17:47:05 +0800

    Add pattern and validation for IpV6 addresses

--- packages/cli/src/cmd/serve.js
+++ packages/cli/src/cmd/serve.js
@@ -31,9 +31,26 @@ function isIPAddressZero(address) {
  * Credits to Danail Gabenski
  */
 function isValidIpAddress(address) {
-  const patternForIp = /^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$/;
+  const patternForIpV4 = /^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?){4}$/;
+  const patternForIpV6 = new RegExp(
+    '^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}'
+  + '|([0-9a-fA-F]{1,4}:){1,7}:'
+  + '|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}'
+  + '|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}'
+  + '|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}'
+  + '|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}'
+  + '|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}'
+  + '|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})'
+  + '|:((:[0-9a-fA-F]{1,4}){1,7}|:)'
+  + '|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}'
+  + '|::(ffff(:0{1,4}){0,1}:){0,1}'
+  + '((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}'
+  + '(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])'
+  + '|([0-9a-fA-F]{1,4}:){1,4}:'
+  + '((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}'
+  + '(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$');
 
-  return patternForIp.test(address);
+  return patternForIpV4.test(address) || patternForIpV6.test(address);
 }
 
 function questionAsync(question) {
