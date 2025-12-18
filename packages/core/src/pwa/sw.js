fb44347b4d607dba6c8348c77796497935fabd75
Harjun751 <Harjun751@gmail.com>
2025-12-18 09:22:57 +0800

    Update cache strategy used in sw.js
    sw.js uses cache-first strategy.
    
    Service worker is installed on localhost scope
    while developing. This means that if building and running
    another website locally, the service worker can still
    return cached values from the previous website the dev
    was working on.
    
    Change to network-first strategy to mitigate this.
    
    Network-first hits the resource first and falls back to cache
    if request fails.

--- packages/core/src/pwa/sw.js
+++ packages/core/src/pwa/sw.js
@@ -2,18 +2,25 @@
 const VERSION = 'v1';
 const CACHE_NAME = `unnamed-markbind-app-${VERSION}`;
 
-async function cacheFirstWithRefresh(request) {
-  const fetchResponsePromise = fetch(request).then(async (networkResponse) => {
+/**
+ * Performs request and updates cache if successful.
+ * Returns cached response if request fails.
+ * Adapted from: https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Guides/Caching
+ */
+async function networkFirst(request) {
+  try {
+    const networkResponse = await fetch(request);
     if (networkResponse.ok) {
       const cache = await caches.open(CACHE_NAME);
       cache.put(request, networkResponse.clone());
     }
     return networkResponse;
-  });
-
-  return (await caches.match(request) || (fetchResponsePromise));
+  } catch (error) {
+    const cachedResponse = await caches.match(request);
+    return cachedResponse || Response.error();
+  }
 }
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(cacheFirstWithRefresh(event.request));
+  event.respondWith(networkFirst(event.request));
 });
