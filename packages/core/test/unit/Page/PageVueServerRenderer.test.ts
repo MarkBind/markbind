b759947a803c5554daa11dee2891e19142d26245
gerteck <changerteck@gmail.com>
2026-01-26 02:36:58 +0800

    Add another testcase

--- packages/core/test/unit/Page/PageVueServerRenderer.test.ts
+++ packages/core/test/unit/Page/PageVueServerRenderer.test.ts
@@ -1,12 +1,21 @@
-import { compileTemplate } from 'vue/compiler-sfc';
-import { pageVueServerRenderer } from '../../../src/Page/PageVueServerRenderer';
-import { PluginManager } from '../../../src/plugins/PluginManager';
-
 jest.mock('../../../src/plugins/PluginManager');
 jest.mock('vue/compiler-sfc');
 jest.mock('fs-extra');
 
 describe('PageVueServerRenderer', () => {
+  let pageVueServerRenderer: any;
+  let PluginManager: any;
+  let compileTemplate: any;
+
+  beforeEach(async () => {
+    jest.resetModules();
+
+    // Re-acquire dependencies for the new module context (As Tags are cached in PluginManager)
+    PluginManager = (await import('../../../src/plugins/PluginManager')).PluginManager;
+    compileTemplate = (await import('vue/compiler-sfc')).compileTemplate;
+    pageVueServerRenderer = (await import('../../../src/Page/PageVueServerRenderer')).pageVueServerRenderer;
+  });
+
   afterEach(() => {
     jest.clearAllMocks();
   });
@@ -54,6 +63,34 @@ describe('PageVueServerRenderer', () => {
       expect(isCustomElement('my-custom-element')).toBe(true);
       expect(isCustomElement('regular-element')).toBe(false);
       expect(isCustomElement('div')).toBe(false);
+    });
+
+    test('handles empty tagConfig or tags without isCustomElement property', async () => {
+      (PluginManager as any).tagConfig = {
+        'some-tag': {
+          isSpecial: false,
+          attributes: [],
+        },
+      };
+
+      (compileTemplate as jest.Mock).mockReturnValue({
+        code: '/* compiled code */',
+      });
+
+      const pageConfig = { sourcePath: 'test.md', resultPath: 'test.html' };
+      const pageAsset = {};
+
+      await pageVueServerRenderer.compileVuePageCreateAndReturnScript(
+        'content',
+        pageConfig as any,
+        pageAsset as any,
+      );
+
+      const { compilerOptions } = (compileTemplate as jest.Mock).mock.calls[0][0];
+      const { isCustomElement } = compilerOptions;
+
+      expect(isCustomElement('some-tag')).toBe(false);
+      expect(isCustomElement('any-tag')).toBe(false);
     });
   });
 });
