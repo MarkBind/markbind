79bcb44c86259444108541299d4a096bf826db55
Thaddaeus Chua <thadchua@gmail.com>
2026-02-04 20:51:27 +0800

    Adapt markdown-it-block-embed/services/VineService to TypeScript

--- packages/core/src/lib/markdown-it/plugins/markdown-it-block-embed/services/VineService.ts
+++ packages/core/src/lib/markdown-it/plugins/markdown-it-block-embed/services/VineService.ts
@@ -1,23 +1,29 @@
 // Copyright (c) Rotorz Limited and portions by original markdown-it-video authors
 // Licensed under the MIT license. See LICENSE file in the project root.
 
-"use strict";
+import VideoServiceBase, { VideoServiceOptions } from './VideoServiceBase';
 
-const VideoServiceBase = require("./VideoServiceBase");
-
+export interface VineOptions extends VideoServiceOptions {
+  width?: number;
+  height?: number;
+  /**
+   * The type of vine embed (e.g., 'simple').
+   */
+  embed?: string;
+}
 
 class VineService extends VideoServiceBase {
 
-  getDefaultOptions() {
+  getDefaultOptions(): VineOptions {
     return { width: 600, height: 600, embed: "simple" };
   }
 
-  extractVideoID(reference) {
+  extractVideoID(reference: string): string {
     let match = reference.match(/^http(?:s?):\/\/(?:www\.)?vine\.co\/v\/([a-zA-Z0-9]{1,13}).*/);
     return match && match[1].length === 11 ? match[1] : reference;
   }
 
-  getVideoUrl(videoID) {
+  getVideoUrl(videoID: string): string {
     let escapedVideoID = this.env.md.utils.escapeHtml(videoID);
     let escapedEmbed = this.env.md.utils.escapeHtml(this.options.embed);
     return `//vine.co/v/${escapedVideoID}/embed/${escapedEmbed}`;
