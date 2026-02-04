91ffcfe2a36c1031e3d83542e08e0c88d68dfcdb
Thaddaeus Chua <thadchua@gmail.com>
2026-02-04 20:59:43 +0800

    Adapt markdown-it-block-embed/services/SlideShareService to TypeScript

--- packages/core/src/lib/markdown-it/plugins/markdown-it-block-embed/services/SlideShareService.ts
+++ packages/core/src/lib/markdown-it/plugins/markdown-it-block-embed/services/SlideShareService.ts
@@ -1,18 +1,21 @@
-"use strict";
+import VideoServiceBase, { VideoServiceOptions } from './VideoServiceBase';
 
-const VideoServiceBase = require("./VideoServiceBase");
+export interface SlideShareOptions extends VideoServiceOptions {
+  width?: number;
+  height?: number;
+}
 
 class SlideShareService extends VideoServiceBase {
 
-  getDefaultOptions() {
+  getDefaultOptions(): SlideShareOptions {
     return {width: 599, height: 487};
   }
 
-  extractVideoID(reference) {
+  extractVideoID(reference: string): string {
     return reference;
   }
 
-  getVideoUrl(videoID) {
+  getVideoUrl(videoID: string): string {
     let escapedVideoID = this.env.md.utils.escapeHtml(videoID);
     return `//www.slideshare.net/slideshow/embed_code/key/${escapedVideoID}`;
   }
