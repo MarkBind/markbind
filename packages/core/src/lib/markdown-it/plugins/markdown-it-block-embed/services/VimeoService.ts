95539cc871723bce703af1fca2df53b4bc9978ee
Thaddaeus Chua <thadchua@gmail.com>
2026-02-04 20:43:02 +0800

    Adapt markdown-it-block-embed/services/VimeoService to TypeScript

--- packages/core/src/lib/markdown-it/plugins/markdown-it-block-embed/services/VimeoService.ts
+++ packages/core/src/lib/markdown-it/plugins/markdown-it-block-embed/services/VimeoService.ts
@@ -1,28 +1,27 @@
 // Copyright (c) Rotorz Limited and portions by original markdown-it-video authors
 // Licensed under the MIT license. See LICENSE file in the project root.
 
-"use strict";
+import VideoServiceBase, { VideoServiceOptions } from "./VideoServiceBase";
 
-const VideoServiceBase = require("./VideoServiceBase");
+interface VimeoOptions extends VideoServiceOptions {
+  width?: number;
+  height?: number;
+}
 
+export default class VimeoService extends VideoServiceBase {
 
-class VimeoService extends VideoServiceBase {
-
-  getDefaultOptions() {
+  getDefaultOptions(): VimeoOptions {
     return { width: 500, height: 281 };
   }
 
-  extractVideoID(reference) {
+  extractVideoID(reference: string): string {
     let match = reference.match(/https?:\/\/(?:www\.|player\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/([^\/]*)\/videos\/|album\/(\d+)\/video\/|)(\d+)(?:$|\/|\?)/);
     return match && typeof match[3] === "string" ? match[3] : reference;
   }
 
-  getVideoUrl(videoID) {
+  getVideoUrl(videoID: string): string {
     let escapedVideoID = this.env.md.utils.escapeHtml(videoID);
     return `//player.vimeo.com/video/${escapedVideoID}`;
   }
 
 }
-
-
-module.exports = VimeoService;
