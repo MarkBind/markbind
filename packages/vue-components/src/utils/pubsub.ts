4e7990f6a47da60ccfcc71d19c629ad9e9805a6d
yihao <yihao039@gmail.com>
2026-02-11 15:28:22 +0800

    Adapt vue-components/src/utils/pubsub to TypeScript

--- packages/vue-components/src/utils/pubsub.ts
+++ packages/vue-components/src/utils/pubsub.ts
@@ -1,13 +1,13 @@
-const subscribers = {};
+const subscribers: Record<string, (() => void)[]> = {};
 
-export function subscribe(event, handler) {
+export function subscribe(event: string, handler: () => void): void {
   if (!subscribers[event]) {
     subscribers[event] = [];
   }
   subscribers[event].push(handler);
 }
 
-export function publish(event) {
+export function publish(event: string): void {
   if (!subscribers[event]) {
     return;
   }
