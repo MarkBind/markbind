b170d13f541ff631367f754c316e62061c4b3fe9
gerteck <changerteck@gmail.com>
2026-03-13 22:07:34 +0800

    Fix lint error

--- packages/core/src/Page/index.ts
+++ packages/core/src/Page/index.ts
@@ -592,9 +592,6 @@ export class Page {
      *
      * However, for automated testings (e.g. snapshots), we will output the pre SSR-processed HTML content
      * as we want to retain the unrendered DOM for easier reference and checking.
-     * 
-     * Additionally, we log the page rendered for easier diagnosis if there are vue render warnings
-     * So that site author can better diagnose page containing vue render warning. 
      */
     logger.verbose(`Rendering page: ${this.pageConfig.sourcePath}`);
     const vueSsrHtml = await pageVueServerRenderer.renderVuePage(renderFn);
