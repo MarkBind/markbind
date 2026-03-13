696595e6594293c466c5f55d48946a1fc0f00770
Arjun <57029562+Harjun751@users.noreply.github.com>
2026-03-13 14:22:11 +0800

    Upgrade winston dependency to v3 (#2858)
    * Upgrade winton to v3 for proper ESM support 
    * Add configuration for dailyrotate audit file
    * Update winston in core to v3
    * Standardize file format logging
    * Add log error message format into log

--- packages/cli/src/util/logger.ts
+++ packages/cli/src/util/logger.ts
@@ -1,28 +1,44 @@
 import chalk from 'chalk';
 import figlet from 'figlet';
+import winston from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
 
-import { winston, transports } from './logger-wrapper.cjs';
+const { transports, format } = winston;
+
+const LEVEL_AND_MESSAGE = (info: winston.Logform.TransformableInfo) =>
+  `${info.level}: ${info.stack || info.message}`;
+
+const consoleFormat = format.combine(
+  format.colorize(),
+
+  format.printf(LEVEL_AND_MESSAGE),
+);
+
+const fileFormat = format.combine(
+  format.timestamp(),
+  format.printf(info => `${info.timestamp} - ${LEVEL_AND_MESSAGE(info)}`),
+);
 
 // @markbind/core's consoleTransport but with level: info
 const consoleTransport = new transports.Console({
-  colorize: true,
+  format: consoleFormat,
   handleExceptions: true,
-  humanReadableUnhandledException: true,
   level: 'info',
-  showLevel: true,
 });
 
 function useDebugConsole(): void {
   consoleTransport.level = 'debug';
 }
 
-const dailyRotateFileTransport = new transports.DailyRotateFile({
+const dailyRotateFileTransport = new DailyRotateFile({
+  format: fileFormat,
   datePattern: 'YYYY-MM-DD',
   dirname: '_markbind/logs',
   filename: 'markbind-%DATE%.log',
   handleExceptions: true,
   level: 'debug',
   maxFiles: 5,
+  auditFile: '_markbind/logs/audit.json',
 });
 
 // Reconfigure the default instance logger winston provides with DailyRotateFile for markbind-cli
