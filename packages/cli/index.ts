40df5b706ed1e05d95dc4e730d27aaf8a9b405d4
Ger Teck <changerteck@gmail.com>
2026-03-16 09:11:17 +0800

    Upgrade commander to v14 and refresh CLI visuals
    Remove serve -d from user facing help
    Update CLI colors

--- packages/cli/index.ts
+++ packages/cli/index.ts
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
 // Entry file for MarkBind project
-import { program } from 'commander';
+import { program, Option } from 'commander';
+import chalk from 'chalk';
 import * as logger from './src/util/logger.js';
 import { build } from './src/cmd/build.js';
 import { deploy } from './src/cmd/deploy.js';
@@ -24,7 +25,12 @@ function printHeader() {
 
 program
   .addHelpText('beforeAll', printHeader())
-  .showHelpAfterError('(run "markbind --help" to list commands)');
+  .showHelpAfterError('(run "markbind --help" to list commands)')
+  .configureHelp({
+    styleTitle: str => chalk.bold.cyan(str),
+    styleUsage: str => chalk.white(str),
+    styleOptionText: str => chalk.yellow(str),
+  });
 
 program
   .allowUnknownOption()
@@ -35,30 +41,56 @@ program
   .version(CLI_VERSION);
 
 program
+  .commandsGroup('Setup Commands')
   .command('init [root]')
   .option('-c, --convert', 'convert a GitHub wiki or docs folder to a MarkBind website')
   .option('-t, --template <type>', 'initialise markbind with a specified template', 'default')
   .alias('i')
+  .summary('init a markbind site')
   .description('init a markbind website project')
   .action((root, options) => {
     init(root, options);
   });
 
 program
+  .commandsGroup('Site Commands')
   .command('serve [root]')
   .alias('s')
-  .option('-f, --force-reload', 'force a full reload of all site files when a file is changed')
-  .option('-n, --no-open', 'do not automatically open the site in browser')
-  .option('-o, --one-page [file]', 'build and serve only a single page in the site initially,'
-      + 'building more pages when they are navigated to. Also lazily rebuilds only the page being viewed when'
-      + 'there are changes to the source files (if needed), building others when navigated to')
-  .option('-b, --background-build', 'when --one-page is specified, enhances one-page serve by building'
-      + 'remaining pages in the background')
-  .option('-p, --port <port>', 'port for server to listen on (Default is 8080)')
-  .option('-s, --site-config <file>', 'specify the site config file (default: site.json)')
-  .option('-d, --dev', 'development mode, enabling live & hot reload for frontend source files.')
-  .option('-a, --address <address>', 'specify the server address/host (Default is 127.0.0.1)')
-  .description('build then serve a website from a directory')
+  .summary('Build then serve a website from a directory')
+  .description('Build and serve a website from a directory')
+
+  .optionsGroup('Build Options')
+  .addOption(
+    program.createOption('-f, --force-reload',
+                         'force a full reload of all site files when a file is changed')
+      .conflicts('onePage'))
+
+  .addOption(
+    program.createOption('-o, --one-page [file]',
+                         'build and serve only a single page in the site initially, '
+                         + 'building more pages when they are navigated to. Also lazily rebuilds only '
+                         + 'the page being viewed when there are changes to the source files (if needed), '
+                         + 'building others when navigated to'))
+
+  .addOption(
+    program.createOption('-b, --background-build',
+                         'when --one-page is specified, enhances one-page serve by building '
+                         + 'remaining pages in the background'))
+
+  .optionsGroup('Server Options')
+  .addOption(
+    program.createOption('-n, --no-open', 'do not automatically open the site in browser'))
+  .addOption(
+    program.createOption('-p, --port <port>', 'port for server to listen on (Default is 8080)'))
+  .addOption(
+    program.createOption('-a, --address <address>', 'specify the server address/host (Default is 127.0.0.1)'))
+  .addOption(
+    program.createOption('-s, --site-config <file>', 'specify the site config file (default: site.json)'))
+
+  // Development mode is hidden as it is not user facing and only works during local development
+  .addOption(
+    new Option('-d, --dev', 'development mode, enabling live & hot reload for frontend source files.')
+      .hideHelp())
   .action((userSpecifiedRoot, options) => {
     serve(userSpecifiedRoot, options);
   });
@@ -69,7 +101,8 @@ program
   .option('--baseUrl [baseUrl]',
           'optional flag which overrides baseUrl in site.json, leave argument empty for empty baseUrl')
   .option('-s, --site-config <file>', 'specify the site config file (default: site.json)')
-  .description('build a website')
+  .summary('Build a website')
+  .description('Build a website')
   .action((userSpecifiedRoot, output, options) => {
     build(userSpecifiedRoot, output, options);
   });
@@ -80,7 +113,8 @@ program
   .option('-c, --ci [githubTokenName]', 'deploy the site in CI Environment [GITHUB_TOKEN]')
   .option('-n, --no-build', 'do not automatically build the site before deployment')
   .option('-s, --site-config <file>', 'specify the site config file (default: site.json)')
-  .description('deploy the latest build of the site to the repo\'s Github pages')
+  .summary('Deploy the latest build of the site to GitHub Pages')
+  .description('Deploy the latest build of the site to the repo\'s GitHub Pages')
   .action((userSpecifiedRoot, options) => {
     deploy(userSpecifiedRoot, options);
   });
