d28819ed79b063c324d2915d6b4472043918fb8b
yihao <yihao039@gmail.com>
2026-03-25 12:52:21 +0800

    Update imports for esm

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -2,4 +2,4 @@ import { Site } from './src/Site/index.js';
 import { Template } from './src/Site/template.js';
 
 export { Site, Template };
-export type { DeployResult } from './src/Site/SiteDeployManager';
+export type { DeployResult } from './src/Site/SiteDeployManager.js';
