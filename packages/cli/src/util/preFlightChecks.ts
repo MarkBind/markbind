661cf618efd154aedeecae53ef954d5a0832bb21
Harjun751 <Harjun751@gmail.com>
2026-04-16 17:28:18 +0800

    Add edge case check in preFlightChecks

--- packages/cli/src/util/preFlightChecks.ts
+++ packages/cli/src/util/preFlightChecks.ts
@@ -3,15 +3,20 @@ import * as logger from './logger.js';
 import packageJson from '../../package.json' with { type: 'json' };
 
 const CLI_VERSION = packageJson.version;
+const REQUIRED_NODE_VERSION = '22.12.0';
 
 function checkNode() {
   const nodeVersion = process.version;
-  const version = nodeVersion.slice(1);
+  const version = semver.coerce(nodeVersion);
 
-  if (semver.lt(version, '22.12.0')) {
+  if (version === null) {
+    logger.warn('Unknown version of node detected.');
+    return;
+  }
+  if (semver.lt(version, REQUIRED_NODE_VERSION)) {
     logger.error(
-      `Markbind ${CLI_VERSION} requires Node >=22.12.0. `
-        + 'Please update Node or use an older version of MarkBind!'
+      `Markbind ${CLI_VERSION} requires node >=${REQUIRED_NODE_VERSION}. `
+        + 'Please update node or use an older version of Markbind!',
     );
     process.exit(1);
   }
